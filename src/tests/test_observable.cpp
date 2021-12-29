96446a1940fb686c70509b08e8406a0d10106996
Aleksey Loginov <victimsnino@gmail.com>
2021-12-29 14:13:01 +0300

    Compile fix

--- src/tests/test_observable.cpp
+++ src/tests/test_observable.cpp
@@ -22,8 +22,7 @@
 
 #include "copy_count_tracker.h"
 
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_all.hpp>
 #include <rpp/observable.h>
 #include <rpp/observer.h>
 #include <rpp/subscriber.h>
