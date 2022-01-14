698c5c8bba814844a7d39f34df47998ff653ed40
github-action-benchmark <github@users.noreply.github.com>
2022-01-14 21:37:40 +0000

    add Catch2 Benchmark Linux CLANG (catch2) benchmark result for e0575b5afe7ebfccac9fcd04bfcede0fcbbcea75

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642196257275,
+  "lastUpdate": 1642196260045,
   "repoUrl": "https://github.com/victimsnino/ReactivePlusPlus",
   "entries": {
     "Catch2 Benchmark Linux GCC": [
@@ -76,6 +76,84 @@ window.BENCHMARK_DATA = {
             "range": "± 2.44616",
             "unit": "ns",
             "extra": "100 samples\n719 iterations"
+          }
+        ]
+      }
+    ],
+    "Catch2 Benchmark Linux CLANG": [
+      {
+        "commit": {
+          "author": {
+            "name": "victimsnino",
+            "username": "victimsnino"
+          },
+          "committer": {
+            "name": "victimsnino",
+            "username": "victimsnino"
+          },
+          "id": "e0575b5afe7ebfccac9fcd04bfcede0fcbbcea75",
+          "message": "Debug bench",
+          "timestamp": "2022-01-14T20:51:51Z",
+          "url": "https://github.com/victimsnino/ReactivePlusPlus/pull/11/commits/e0575b5afe7ebfccac9fcd04bfcede0fcbbcea75"
+        },
+        "date": 1642196259150,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Construction",
+            "value": 112.225,
+            "range": "± 9.4648",
+            "unit": "ns",
+            "extra": "100 samples\n332 iterations"
+          },
+          {
+            "name": "Subscribe",
+            "value": 30.6051,
+            "range": "± 4.01891",
+            "unit": "ns",
+            "extra": "100 samples\n1039 iterations"
+          },
+          {
+            "name": "OnNext",
+            "value": 2.0109,
+            "range": "± 0.0283315",
+            "unit": "ns",
+            "extra": "100 samples\n18230 iterations"
+          },
+          {
+            "name": "Raw call to lambda",
+            "value": 0.776624,
+            "range": "± 0.0975566",
+            "unit": "ns",
+            "extra": "100 samples\n45118 iterations"
+          },
+          {
+            "name": "Call to lambda via std::function",
+            "value": 2.05248,
+            "range": "± 0.237658",
+            "unit": "ns",
+            "extra": "100 samples\n18268 iterations"
+          },
+          {
+            "name": "Copy  lambda",
+            "value": 8.31594,
+            "range": "± 0.898642",
+            "unit": "ns",
+            "extra": "100 samples\n3870 iterations"
+          },
+          {
+            "name": "Copy  std::function",
+            "value": 31.2605,
+            "range": "± 4.33114",
+            "unit": "ns",
+            "extra": "100 samples\n1093 iterations"
+          },
+          {
+            "name": "Make shared  copy of lambda",
+            "value": 26.9335,
+            "range": "± 4.34647",
+            "unit": "ns",
+            "extra": "100 samples\n1253 iterations"
           }
         ]
       }
