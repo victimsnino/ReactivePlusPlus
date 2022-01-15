d7a522dd29c1377a11a3a3d74afb24235b2960cd
github-action-benchmark <github@users.noreply.github.com>
2022-01-15 19:36:55 +0000

    add Catch2 Benchmark Linux GCC (catch2) benchmark result for f6c13f5153ee7cdb2ff4bdbfbfccd47383af0127

---
+++ dev/bench/data.js
@@ -0,0 +1,49 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1642275415331,
+  "repoUrl": "https://github.com/victimsnino/ReactivePlusPlus",
+  "entries": {
+    "Catch2 Benchmark Linux GCC": [
+      {
+        "commit": {
+          "author": {
+            "name": "victimsnino",
+            "username": "victimsnino"
+          },
+          "committer": {
+            "name": "victimsnino",
+            "username": "victimsnino"
+          },
+          "id": "f6c13f5153ee7cdb2ff4bdbfbfccd47383af0127",
+          "message": "Debug bench",
+          "timestamp": "2022-01-14T20:51:51Z",
+          "url": "https://github.com/victimsnino/ReactivePlusPlus/pull/11/commits/f6c13f5153ee7cdb2ff4bdbfbfccd47383af0127"
+        },
+        "date": 1642275414490,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Construction",
+            "value": 200.192,
+            "range": "± 30.9308",
+            "unit": "ns",
+            "extra": "100 samples\n146 iterations"
+          },
+          {
+            "name": "Subscribe",
+            "value": 33.0834,
+            "range": "± 12.2405",
+            "unit": "ns",
+            "extra": "100 samples\n948 iterations"
+          },
+          {
+            "name": "OnNext",
+            "value": 0.61675,
+            "range": "± 0.20919",
+            "unit": "ns",
+            "extra": "100 samples\n46557 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
