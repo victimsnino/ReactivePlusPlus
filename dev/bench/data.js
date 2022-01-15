4e0f650a906734ac4e0339781de167c11ff6f2d7
github-action-benchmark <github@users.noreply.github.com>
2022-01-15 19:50:32 +0000

    add Catch2 Benchmark Linux CLANG (catch2) benchmark result for 314e49c2a22a537a1f5f59556f4ad2e979734ade

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642275633944,
+  "lastUpdate": 1642276232707,
   "repoUrl": "https://github.com/victimsnino/ReactivePlusPlus",
   "entries": {
     "Catch2 Benchmark Linux GCC": [
@@ -125,6 +125,51 @@ window.BENCHMARK_DATA = {
             "range": "± 0.0260532",
             "unit": "ns",
             "extra": "100 samples\n18688 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "32845901+victimsnino@users.noreply.github.com",
+            "name": "Aleksey Loginov",
+            "username": "victimsnino"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "314e49c2a22a537a1f5f59556f4ad2e979734ade",
+          "message": "Debug bench (#11)",
+          "timestamp": "2022-01-15T22:49:41+03:00",
+          "tree_id": "71bacf76fb16fb2698e890b6549aa473d461768d",
+          "url": "https://github.com/victimsnino/ReactivePlusPlus/commit/314e49c2a22a537a1f5f59556f4ad2e979734ade"
+        },
+        "date": 1642276232303,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Construction",
+            "value": 95.8296,
+            "range": "± 12.1075",
+            "unit": "ns",
+            "extra": "100 samples\n322 iterations"
+          },
+          {
+            "name": "Subscribe",
+            "value": 32.8745,
+            "range": "± 2.69362",
+            "unit": "ns",
+            "extra": "100 samples\n939 iterations"
+          },
+          {
+            "name": "OnNext",
+            "value": 1.96243,
+            "range": "± 0.190098",
+            "unit": "ns",
+            "extra": "100 samples\n18267 iterations"
           }
         ]
       }
