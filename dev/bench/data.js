19c68e982e74c0f5f34ccb5669e40d85efa09ee4
github-action-benchmark <github@users.noreply.github.com>
2022-03-08 13:54:11 +0000

    add Catch2 Benchmark Linux GCC (catch2) benchmark result for 516ef8e40510613a7017fc590658f36848ecc3b9

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1646747651756,
+  "repoUrl": "https://github.com/victimsnino/ReactivePlusPlus",
+  "entries": {
+    "Catch2 Benchmark Linux GCC": [
+      {
+        "commit": {
+          "author": {
+            "email": "32845901+victimsnino@users.noreply.github.com",
+            "name": "Aleksey Loginov",
+            "username": "victimsnino"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "516ef8e40510613a7017fc590658f36848ecc3b9",
+          "message": "Refactor benchmarks (#19)",
+          "timestamp": "2022-03-08T16:53:17+03:00",
+          "tree_id": "8d3211143787d5a9890a5418e3d846522a5634dc",
+          "url": "https://github.com/victimsnino/ReactivePlusPlus/commit/516ef8e40510613a7017fc590658f36848ecc3b9"
+        },
+        "date": 1646747651334,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Specific observable construction",
+            "value": 0.804962,
+            "range": "± 0.02828",
+            "unit": "ns",
+            "extra": "100 samples\n35796 iterations"
+          },
+          {
+            "name": "Dynamic observable construction",
+            "value": 17.4917,
+            "range": "± 0.862392",
+            "unit": "ns",
+            "extra": "100 samples\n1640 iterations"
+          }
+        ]
+      }
+    ]
+  }
+}
