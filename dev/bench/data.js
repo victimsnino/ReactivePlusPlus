636832e8e8154bea3461a6b0c82d653058336868
github-action-benchmark <github@users.noreply.github.com>
2022-03-08 13:54:15 +0000

    add Catch2 Benchmark Linux CLANG (catch2) benchmark result for 516ef8e40510613a7017fc590658f36848ecc3b9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1646747651756,
+  "lastUpdate": 1646747655894,
   "repoUrl": "https://github.com/victimsnino/ReactivePlusPlus",
   "entries": {
     "Catch2 Benchmark Linux GCC": [
@@ -38,6 +38,46 @@ window.BENCHMARK_DATA = {
             "range": "± 0.862392",
             "unit": "ns",
             "extra": "100 samples\n1640 iterations"
+          }
+        ]
+      }
+    ],
+    "Catch2 Benchmark Linux CLANG": [
+      {
+        "commit": {
+          "author": {
+            "email": "32845901+victimsnino@users.noreply.github.com",
+            "name": "Aleksey Loginov",
+            "username": "victimsnino"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "516ef8e40510613a7017fc590658f36848ecc3b9",
+          "message": "Refactor benchmarks (#19)",
+          "timestamp": "2022-03-08T16:53:17+03:00",
+          "tree_id": "8d3211143787d5a9890a5418e3d846522a5634dc",
+          "url": "https://github.com/victimsnino/ReactivePlusPlus/commit/516ef8e40510613a7017fc590658f36848ecc3b9"
+        },
+        "date": 1646747655462,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Specific observable construction",
+            "value": 1.00438,
+            "range": "± 0.0131559",
+            "unit": "ns",
+            "extra": "100 samples\n29963 iterations"
+          },
+          {
+            "name": "Dynamic observable construction",
+            "value": 18.7665,
+            "range": "± 0.255645",
+            "unit": "ns",
+            "extra": "100 samples\n1575 iterations"
           }
         ]
       }
