7d8070b64ea1e665c651f6eee4fdeb391dbd4374
Aleksey Loginov <32845901+victimsnino@users.noreply.github.com>
2022-03-10 00:23:38 +0300

    Update compare_results.py

--- ci/compare_results.py
+++ ci/compare_results.py
@@ -7,12 +7,14 @@ for name, tests in results.items():
     print(f"# {name}")
     print("<details>")
     print("<summary>Table</summary>")
+    print("")
     print("Test Name | Current, ns | Prev, ns | Ratio")
     print("--- | --- | --- | ---")
     for test_name, data in tests.items():
         new_value = f"{data[-1]['val']}ns"
-        old_value = f"{data[-2]['val']}ns" if len(data)> 1 else '-'
-        ratio     = data[-1]['val']/data[-2]['val'] if len(data)> 1 else '-'
+        old_value = f"{data[-2]['val']}ns" if len(data)> 1 else '.'
+        ratio     = data[-1]['val']/data[-2]['val'] if len(data)> 1 else '.'
         print(f"{test_name} | { new_value } | { old_value } | {ratio}")
+    print("")
     print("</details>")
     print("")
