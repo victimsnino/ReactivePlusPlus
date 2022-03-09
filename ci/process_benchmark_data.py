112238a94d0742a86abeef9ae155a196ea09f4d7
Aleksey Loginov <victimsnino@gmail.com>
2022-03-09 20:51:53 +0300

    test

--- ci/process_benchmark_data.py
+++ ci/process_benchmark_data.py
@@ -3,7 +3,7 @@ import json
 import sys
 
 git_commit     = sys.argv[1] 
-git_commit_url = sys.argv[2] 
+# git_commit_url = sys.argv[2] 
 
 new_results = {}
 for file in os.listdir(os.fsencode("./artifacts")):
@@ -19,7 +19,7 @@ for name, vals in new_results.items():
     old_results.setdefault(name, {})
     for test_name, val in vals.items():
         old_results[name].setdefault(test_name, [])
-        old_results[name][test_name].append({"hash" : git_commit, "url" : git_commit_url, "val" :val})
+        old_results[name][test_name].append({"hash" : git_commit, "url" : "zero", "val" :val})
         
 with open("./gh-pages/results.json", 'w') as f:
     json.dump(old_results, f)
