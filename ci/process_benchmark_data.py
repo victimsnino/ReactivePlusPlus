1fc3ee9b04e6bb6954a0ae59e27279a6049511d2
Aleksey Loginov <victimsnino@gmail.com>
2022-03-09 20:10:56 +0300

    Continue

--- ci/process_benchmark_data.py
+++ ci/process_benchmark_data.py
@@ -4,14 +4,28 @@ from plotly.subplots import make_subplots
 
 import os
 import json
+import sys
+git_commit = sys.arv[1] 
 
 new_results = {}
 for file in os.listdir(os.fsencode("./artifacts")):
      folder_with_results = os.fsdecode(file)
-     with open('folder_with_results/results.json', 'r') as f:
+     with open(f'./{folder_with_results}/results.json', 'r') as f:
         new_results[folder_with_results] = json.load(f)
 
-print(new_results)
+with open("./gh-pages/results.json", 'r') as f:
+    old_results = json.load(f)
+
+for name, vals in new_results.items():
+    old_results.setdefault(name, {})
+    for test_name, val in vals.items():
+        old_results[name].setdefault(test_name, [])
+        old_results[name][test_name].append([git_commit, val])
+        
+with open("gh-pages/results.json", 'w') as f:
+    json.dump(old_results, f)
+
+print(old_results)
 
 # filename = "result.html"
 # dashboard = open(filename, 'w')
