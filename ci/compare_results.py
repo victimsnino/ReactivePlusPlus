3cc996124e49736370f51b01ca6130f94d137237
Aleksey Loginov <victimsnino@gmail.com>
2022-03-10 00:01:11 +0300

    fix logging

--- ci/compare_results.py
+++ ci/compare_results.py
@@ -5,9 +5,9 @@ with open("./gh-pages/results.json", 'r') as f:
 
 for name, tests in results.items():
     print(f"# {name}")
-    print("Test Name | Prev | Current")
+    print("Test Name | Current | Prev")
     print("--- | --- | ---")
     for test_name, data in tests.items():
-        print(f"{test_name} | { data[-2]['val'] } | { data[-1]['val'] }")
+        print(f"{test_name} | { data[-1]['val'] } | { data[-2]['val'] if len(data)> 1 else '-' }")
     
     print("")
