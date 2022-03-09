b7108fa560500e6d07dc68594c03e79a1a68a057
Aleksey Loginov <victimsnino@gmail.com>
2022-03-10 00:06:23 +0300

    ratio

--- ci/compare_results.py
+++ ci/compare_results.py
@@ -5,9 +5,12 @@ with open("./gh-pages/results.json", 'r') as f:
 
 for name, tests in results.items():
     print(f"# {name}")
-    print("Test Name | Current | Prev")
+    print("Test Name | Current, ns | Prev, ns | Ratio")
     print("--- | --- | ---")
     for test_name, data in tests.items():
+        new_value = data[-1]['val']
+        old_value = data[-2]['val'] if len(data)> 1 else '-'
+        ration = new_value/old_value if old_value != '-' else '-'
         print(f"{test_name} | { data[-1]['val'] } | { data[-2]['val'] if len(data)> 1 else '-' }")
     
     print("")
