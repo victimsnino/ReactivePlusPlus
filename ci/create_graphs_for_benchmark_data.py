8519514bfa71d56d02002bd724eda8b7855227c3
Aleksey Loginov <victimsnino@gmail.com>
2022-03-09 20:28:33 +0300

    graphs

---
+++ ci/create_graphs_for_benchmark_data.py
@@ -0,0 +1,37 @@
+import plotly.offline as pyo
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import json
+
+
+
+dashboard = open("./gh-pages/benchmark.html", 'w')
+dashboard.write("<html><head></head><body>" + "\n")
+add_js = True
+
+def dump_plot(fig):
+    global add_js
+    global dashboard
+
+    dashboard.write(pyo.plot(fig, include_plotlyjs=add_js, output_type='div'))
+    add_js = False
+
+
+with open("./gh-pages/results.json", 'r') as f:
+    results = json.load(f)  
+
+for name, tests in results.items():
+    fig = make_subplots(rows=len(tests), cols=1, shared_xaxes=True, vertical_spacing=0.01)
+
+    for test_name, data in tests.items():
+        x = [v["hash"] for v in data]
+        y = [v["val"] for v in data]
+        fig.add_trace(go.Scatter(x=x, y=y, name=test_name), row=3, col=1,)
+
+    fig.update_layout(title_text="name", title_x=0.5)
+    fig.update_xaxes(title_text='Commit')
+    fig.update_yaxes(title_text='Duration')
+    dump_plot(fig)
+
+dashboard.write("</body></html>" + "\n")
+dashboard.close()
--- ci/process_benchmark_data.py
+++ ci/process_benchmark_data.py
@@ -11,8 +11,9 @@ for file in os.listdir(os.fsencode("./artifacts")):
      with open(f'./{folder_with_results}/results.json', 'r') as f:
         new_results[folder_with_results] = json.load(f)
 
+old_results = {}
 with open("./gh-pages/results.json", 'r') as f:
-    old_results = json.load(f)
+    old_results = json.load(f)   
 
 for name, vals in new_results.items():
     old_results.setdefault(name, {})
@@ -20,7 +21,7 @@ for name, vals in new_results.items():
         old_results[name].setdefault(test_name, [])
         old_results[name][test_name].append({"hash" : git_commit, "url" : git_commit_url, "val" :val})
         
-with open("gh-pages/results.json", 'w') as f:
+with open("./gh-pages/results.json", 'w') as f:
     json.dump(old_results, f)
 
 print(old_results)
