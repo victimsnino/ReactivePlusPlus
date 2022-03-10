b812f5dd66bf0e55f5b016fea65e26efa3fce26a
Aleksey Loginov <victimsnino@gmail.com>
2022-03-10 18:03:08 +0300

    fix

--- ci/parse_benchmark_to_json.py
+++ ci/parse_benchmark_to_json.py
@@ -3,16 +3,16 @@ import json
 import sys
 import pandas as pd
 
-results = sys.argv[1]
 
-root = ET.parse(results).getroot()
+def parse(results):
+    root = ET.parse(results).getroot()
 
-results = pd.DataFrame()
+    results = pd.DataFrame()
 
-for test_case in root.findall("TestCase"):
-    for result in test_case.findall("BenchmarkResults"):
-        data = {"benchmark_name" : test_case.get("name"), "test_case" : result.get("name"), **{k:v for k, v in result.find('mean').items()}}
-        results = pd.concat([results, pd.DataFrame(data=data, index=[0])]).reset_index(drop=True)
+    for test_case in root.findall("TestCase"):
+        for result in test_case.findall("BenchmarkResults"):
+            data = {"benchmark_name" : test_case.get("name"), "test_case" : result.get("name"), **{k:v for k, v in result.find('mean').items()}}
+            results = pd.concat([results, pd.DataFrame(data=data, index=[0])]).reset_index(drop=True)
 
-results.index.name = "id"
-results.to_csv("parsed.csv")
+    results.index.name = "id"
+    return results
--- ci/process_benchmark_data.py
+++ ci/process_benchmark_data.py
@@ -2,16 +2,17 @@ import os
 import json
 import sys
 import pandas as pd
+from parse_benchmark_to_json import parse
 
 git_commit     = str(sys.argv[1]) 
-commit_message = sys.argv[2].split("\n")[0] if len(sys.argv) > 2 else "Current PRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR"
+commit_message = sys.argv[2].split("\n")[0] if len(sys.argv) > 2 else "Current PR"
 
 new_results = pd.DataFrame()
 for file in os.listdir(os.fsencode("./artifacts")):
     folder_with_results = os.fsdecode(file)
-    new_data = pd.read_csv(f'./artifacts/{folder_with_results}/parsed.csv', index_col="id")
+    new_data = parse(f'./artifacts/{folder_with_results}/benchmark_result.txt')
     new_data["platform"] = [folder_with_results]*len(new_data)
-    new_data["commit"] = [f"{commit_message[:20]}{"..." if len(commit_message)> 20 else ""} ({git_commit})"]*len(new_data)
+    new_data["commit"] = [f"{commit_message[:20]}{'...' if len(commit_message)> 20 else ''} ({git_commit})"]*len(new_data)
     new_results = pd.concat([new_results, new_data]).reset_index(drop=True)
 
 old_results = pd.DataFrame()
