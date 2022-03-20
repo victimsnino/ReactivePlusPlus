a50518b03c1e529311696c82cdbf29787116901f
victimsnino <victimsnino@users.noreply.github.com>
2022-03-20 20:53:34 +0000

    deploy: cd2c8acab5dcee9db6acefd304b0638e8cae4273

--- docs/html/search/classes_2.js
+++ docs/html/search/classes_2.js
@@ -4,7 +4,5 @@ var searchData=
   ['interface_5fobservable_3c_20type_2c_20dynamic_5fobservable_3c_20type_20_3e_20_3e_1',['interface_observable&lt; Type, dynamic_observable&lt; Type &gt; &gt;',['../structrpp_1_1interface__observable.html',1,'rpp']]],
   ['interface_5fobservable_3c_20type_2c_20specific_5fobservable_3c_20type_2c_20onsubscribefn_20_3e_20_3e_2',['interface_observable&lt; Type, specific_observable&lt; Type, OnSubscribeFn &gt; &gt;',['../structrpp_1_1interface__observable.html',1,'rpp']]],
   ['interface_5fobserver_3',['interface_observer',['../structrpp_1_1interface__observer.html',1,'rpp']]],
-  ['interface_5fobserver_3c_20type_20_3e_4',['interface_observer&lt; Type &gt;',['../structrpp_1_1interface__observer.html',1,'rpp']]],
-  ['is_5fspecific_5fobserver_5',['is_specific_observer',['../structrpp_1_1details_1_1is__specific__observer.html',1,'rpp::details']]],
-  ['is_5fspecific_5fobserver_3c_20specific_5fobserver_3c_20an_2e_2e_2e_20_3e_20_3e_6',['is_specific_observer&lt; specific_observer&lt; An... &gt; &gt;',['../structrpp_1_1details_1_1is__specific__observer_3_01specific__observer_3_01_an_8_8_8_01_4_01_4.html',1,'rpp::details']]]
+  ['interface_5fobserver_3c_20type_20_3e_4',['interface_observer&lt; Type &gt;',['../structrpp_1_1interface__observer.html',1,'rpp']]]
 ];
