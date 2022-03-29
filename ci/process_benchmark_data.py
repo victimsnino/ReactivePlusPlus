902c20dd30be0a0f003e54168d746d7214b3b40a
Aleksey Loginov <victimsnino@gmail.com>
2022-03-29 23:18:48 +0300

    Try to add rxcpp_bench

--- ci/process_benchmark_data.py
+++ ci/process_benchmark_data.py
@@ -10,10 +10,11 @@ commit_message = sys.argv[2].split("\n")[0] if len(sys.argv) > 2 else "Current P
 new_results = pd.DataFrame()
 for file in os.listdir(os.fsencode("./artifacts")):
     folder_with_results = os.fsdecode(file)
-    new_data = parse(f'./artifacts/{folder_with_results}/benchmark_result.txt')
-    new_data["platform"] = [folder_with_results]*len(new_data)
-    new_data["commit"] = [f"{commit_message[:20]}{'...' if len(commit_message)> 20 else ''} ({git_commit})"]*len(new_data)
-    new_results = pd.concat([new_results, new_data]).reset_index(drop=True)
+    for file_name in ['benchmark_result', 'rxcpp_benchmark_result']:
+        new_data = parse(f'./artifacts/{folder_with_results}/{file_name}.txt')
+        new_data["platform"] = [folder_with_results]*len(new_data)
+        new_data["commit"] = [f"{commit_message[:20]}{'...' if len(commit_message)> 20 else ''} ({git_commit})"]*len(new_data)
+        new_results = pd.concat([new_results, new_data]).reset_index(drop=True)
 
 old_results = pd.DataFrame()
 if os.path.exists("./gh-pages/results.csv"):
