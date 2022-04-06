f8b5afe24f9f4980ac7f1fe86a4efc646602620e
Aleksey Loginov <victimsnino@gmail.com>
2022-04-06 16:36:19 +0300

    Try to fix gcc

--- src/include/rpp/schedulers/new_thread_scheduler.h
+++ src/include/rpp/schedulers/new_thread_scheduler.h
@@ -66,6 +66,7 @@ private:
 
 /**
  * \brief scheduler which schedule execution of via queueing tasks to another thread with priority to time_point and order
+ * \details Creates new thread for each "create_worker" call. Any scheduled task will be queued to created thread for execution with respect to time_point and number of task
  */
 class new_thread final : public details::scheduler_tag
 {
