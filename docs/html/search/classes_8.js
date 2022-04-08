360b31638b4a787030f202c78c74256879019fc3
victimsnino <victimsnino@users.noreply.github.com>
2022-04-08 20:56:40 +0000

    deploy: 2c4ed9ee0a2087eab4c8290582bcf1bd52466c5b

--- docs/html/search/classes_8.js
+++ docs/html/search/classes_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['worker_0',['worker',['../classrpp_1_1schedulers_1_1immediate_1_1worker.html',1,'rpp::schedulers::immediate::worker'],['../classrpp_1_1schedulers_1_1new__thread_1_1worker.html',1,'rpp::schedulers::new_thread::worker']]]
+  ['worker_0',['worker',['../classrpp_1_1schedulers_1_1worker.html',1,'rpp::schedulers']]],
+  ['worker_5fstrategy_1',['worker_strategy',['../classrpp_1_1schedulers_1_1immediate_1_1worker__strategy.html',1,'rpp::schedulers::immediate::worker_strategy'],['../classrpp_1_1schedulers_1_1new__thread_1_1worker__strategy.html',1,'rpp::schedulers::new_thread::worker_strategy']]]
 ];
