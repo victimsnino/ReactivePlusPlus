65e70283f0cfb96658ceae2650e711fa1930f8ae
Aleksey Loginov <victimsnino@gmail.com>
2022-04-12 23:13:06 +0300

    extend sample

--- src/samples/basic/basic.cpp
+++ src/samples/basic/basic.cpp
@@ -7,10 +7,9 @@ int main()
     auto observable = rpp::source::create<char>([](const auto& sub)
                       {
                           while (sub.is_subscribed())
-                          {
                               sub.on_next(std::getchar());
-                          }
                       })
+                      .take_while([](char v) { return v != '0'; })
                       .filter(std::not_fn(std::isdigit))
                       .map(std::toupper);
 
