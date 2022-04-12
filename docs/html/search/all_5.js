23468d3e8bfed0c98ad225667f09f181fbb10bc7
victimsnino <victimsnino@users.noreply.github.com>
2022-04-12 09:03:40 +0000

    deploy: c3d75e0e55a3b4d29bcee07547dd697a24df2312

--- docs/html/search/all_5.js
+++ docs/html/search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['just_0',['just',['../group__observables.html#gae236b520ed9da3383b3b54be4dd5dea2',1,'rpp::observable']]],
+  ['just_0',['just',['../group__observables.html#ga4c12eeef18535ffd2b9153524b8a8899',1,'rpp::observable']]],
   ['just_2eh_1',['just.h',['../just_8h.html',1,'']]]
 ];
--- docs/html/search/functions_3.js
+++ docs/html/search/functions_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['just_0',['just',['../group__observables.html#gae236b520ed9da3383b3b54be4dd5dea2',1,'rpp::observable']]]
+  ['just_0',['just',['../group__observables.html#ga4c12eeef18535ffd2b9153524b8a8899',1,'rpp::observable']]]
 ];
