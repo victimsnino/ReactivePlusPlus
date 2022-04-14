8e2960cd3600e45ae3e765ce5c5340ffe93ceef4
Aleksey Loginov <victimsnino@gmail.com>
2022-04-14 13:42:35 +0300

    add tests

--- src/tests/test_from.cpp
+++ src/tests/test_from.cpp
@@ -20,6 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include "copy_count_tracker.h"
 #include "mock_observer.h"
 
 #include <rpp/sources/from.h>
@@ -28,7 +29,7 @@
 SCENARIO("from iterable")
 {
     auto mock = mock_observer<int>();
-    GIVEN("observable from variadic")
+    GIVEN("observable from iterable")
     {
         auto vals = std::vector{1, 2, 3, 4, 5, 6};
         auto obs  = rpp::source::from(vals);
@@ -39,6 +40,68 @@ SCENARIO("from iterable")
             {
                 CHECK(mock.get_received_values() == vals);
                 CHECK(mock.get_on_completed_count() == 1);
+            }
+        }
+    }
+}
+
+SCENARIO("from iterable", "[track_copy]")
+{
+    copy_count_tracker tracker{};
+    auto               vals         = std::array{tracker};
+    auto               initial_copy = tracker.get_copy_count();
+    auto               initial_move = tracker.get_move_count();
+
+    GIVEN("observable from copied iterable")
+    {
+        auto obs = rpp::source::from(vals);
+        WHEN("subscribe on it")
+        {
+            obs.subscribe();
+            THEN("no extra copies")
+            {
+                CHECK(tracker.get_copy_count() - initial_copy == 1); // 1 copy to wrapped container
+                CHECK(tracker.get_move_count() - initial_move == 2); // 1 move to lambda, 1 move lambda to observable
+            }
+        }
+    }
+    GIVEN("observable from moved iterable")
+    {
+        auto obs = rpp::source::from(std::move(vals));
+        WHEN("subscribe on it")
+        {
+            obs.subscribe();
+            THEN("no extra copies")
+            {
+                CHECK(tracker.get_copy_count() - initial_copy == 0); 
+                CHECK(tracker.get_move_count() - initial_move == 3); // 1 move to wrapped container + 1 move to lambda, 1 move lambda to observable
+            }
+        }
+    }
+
+    GIVEN("observable from copied iterable with shared memory model")
+    {
+        auto obs = rpp::source::from<rpp::memory_model::use_shared>(vals);
+        WHEN("subscribe on it")
+        {
+            obs.subscribe();
+            THEN("no extra copies")
+            {
+                CHECK(tracker.get_copy_count() - initial_copy == 1); // 1 copy to shared_ptr
+                CHECK(tracker.get_move_count() - initial_move == 0);
+            }
+        }
+    }
+    GIVEN("observable from moved iterable")
+    {
+        auto obs = rpp::source::from<rpp::memory_model::use_shared>(std::move(vals));
+        WHEN("subscribe on it")
+        {
+            obs.subscribe();
+            THEN("no extra copies")
+            {
+                CHECK(tracker.get_copy_count() - initial_copy == 0);
+                CHECK(tracker.get_move_count() - initial_move == 1); // 1 copy to shared_ptr
             }
         }
     }
