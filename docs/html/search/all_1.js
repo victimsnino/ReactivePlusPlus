f21b028ecf0c87bd3ff7951122f38f3f54fbe977
victimsnino <victimsnino@users.noreply.github.com>
2022-04-15 21:04:37 +0000

    deploy: 4f41be6c0473d3c778ae9818750b3b167eb7c153

--- docs/html/search/all_1.js
+++ docs/html/search/all_1.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['callback_5fsubscription_0',['callback_subscription',['../classrpp_1_1callback__subscription.html',1,'rpp']]],
   ['composite_5fsubscription_1',['composite_subscription',['../classrpp_1_1composite__subscription.html',1,'rpp']]],
-  ['create_2',['create',['../group__observables.html#gaa70991953a96a64c95d7566ff7069f6b',1,'rpp::observable::create(OnSubscribeFn &amp;&amp;on_subscribe)'],['../group__observables.html#ga7c355ad67fb60d523e6d05604638bae2',1,'rpp::observable::create(OnSubscribeFn &amp;&amp;on_subscribe)']]],
+  ['create_2',['create',['../group__observables.html#gaa70991953a96a64c95d7566ff7069f6b',1,'rpp::observable']]],
   ['create_2eh_3',['create.h',['../create_8h.html',1,'']]]
 ];
