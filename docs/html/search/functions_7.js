be6203a8da611d23d07cf1a47682d5f2118f1efb
victimsnino <victimsnino@users.noreply.github.com>
2022-04-15 18:54:39 +0000

    deploy: 5da49ea8e78b0871acd2c2983e3bb675b0f26b10

--- docs/html/search/functions_7.js
+++ docs/html/search/functions_7.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['map_0',['map',['../group__operators.html#gae5dd31e4559517de5cf1145ff2005a86',1,'rpp::details::member_overload&lt; Type, SpecificObservable, map_tag &gt;']]]
+  ['map_0',['map',['../group__operators.html#gae5dd31e4559517de5cf1145ff2005a86',1,'rpp::details::member_overload&lt; Type, SpecificObservable, map_tag &gt;']]],
+  ['merge_1',['merge',['../group__operators.html#ga84ed0dcdeda6a4cb3a9871ab2817e114',1,'rpp::details::member_overload&lt; Type, SpecificObservable, merge_tag &gt;']]]
 ];
