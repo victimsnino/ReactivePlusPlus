bff7b881f98661899ac3f7d652d206215bb9cc35
victimsnino <victimsnino@users.noreply.github.com>
2022-04-18 11:52:10 +0000

    deploy: 36f1dba062ddc64809dcd10efa198f6475e2a248

--- docs/html/search/classes_7.js
+++ docs/html/search/classes_7.js
@@ -7,9 +7,10 @@ var searchData=
   ['specific_5fsubscriber_4',['specific_subscriber',['../classrpp_1_1specific__subscriber.html',1,'rpp']]],
   ['specific_5fsubscriber_3c_20t_2c_20dynamic_5fobserver_3c_20t_20_3e_20_3e_5',['specific_subscriber&lt; T, dynamic_observer&lt; T &gt; &gt;',['../classrpp_1_1specific__subscriber.html',1,'rpp']]],
   ['state_5fobserver_6',['state_observer',['../classrpp_1_1details_1_1state__observer.html',1,'rpp::details']]],
-  ['subscriber_5fbase_7',['subscriber_base',['../classrpp_1_1details_1_1subscriber__base.html',1,'rpp::details']]],
-  ['subscriber_5ftag_8',['subscriber_tag',['../structrpp_1_1details_1_1subscriber__tag.html',1,'rpp::details']]],
-  ['subscription_5fbase_9',['subscription_base',['../classrpp_1_1subscription__base.html',1,'rpp']]],
-  ['subscription_5fguard_10',['subscription_guard',['../classrpp_1_1subscription__guard.html',1,'rpp']]],
-  ['subscription_5fstate_11',['subscription_state',['../classrpp_1_1details_1_1subscription__state.html',1,'rpp::details']]]
+  ['state_5ft_7',['state_t',['../structrpp_1_1details_1_1state__t.html',1,'rpp::details']]],
+  ['subscriber_5fbase_8',['subscriber_base',['../classrpp_1_1details_1_1subscriber__base.html',1,'rpp::details']]],
+  ['subscriber_5ftag_9',['subscriber_tag',['../structrpp_1_1details_1_1subscriber__tag.html',1,'rpp::details']]],
+  ['subscription_5fbase_10',['subscription_base',['../classrpp_1_1subscription__base.html',1,'rpp']]],
+  ['subscription_5fguard_11',['subscription_guard',['../classrpp_1_1subscription__guard.html',1,'rpp']]],
+  ['subscription_5fstate_12',['subscription_state',['../classrpp_1_1details_1_1subscription__state.html',1,'rpp::details']]]
 ];
