0396bffc8a55039231b748b871c1cc496d1dbdf3
Aleksey Loginov <victimsnino@gmail.com>
2022-04-21 12:40:36 +0300

    compile

--- src/tests/test_subjects.cpp
+++ src/tests/test_subjects.cpp
@@ -7,7 +7,7 @@
 // 
 //  Project home: https://github.com/victimsnino/ReactivePlusPlus
 
-#include "mock_observer.h"
+#include "mock_observer.hpp"
 
 #include <catch2/catch_test_macros.hpp>
 #include <rpp/subjects/publish_subject.hpp>
