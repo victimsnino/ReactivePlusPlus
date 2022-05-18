62748df117cb9285137a1a9c12c5ecd48d769cd8
Aleksey Loginov <victimsnino@gmail.com>
2022-05-18 23:12:51 +0300

    examples

--- src/include/rpp/operators/fwd/subscribe_on.hpp
+++ src/include/rpp/operators/fwd/subscribe_on.hpp
@@ -15,7 +15,7 @@ namespace rpp::operators
 /**
  * \copydoc rpp::details::member_overload::subscribe_on
  */
-template<rpp::schedulers::constraint::scheduler TScheduler>
+template<schedulers::constraint::scheduler TScheduler>
 auto subscribe_on(const TScheduler& scheduler) requires details::is_header_included<details::subscribe_on_tag, TScheduler>
 {
     return [scheduler]<constraint::observable TObservable>(TObservable&& observable)
@@ -27,32 +27,31 @@ auto subscribe_on(const TScheduler& scheduler) requires details::is_header_inclu
 
 namespace rpp::details
 {
-template<constraint::decayed_type Type, constraint::observable_of_type<Type> TObs, rpp::schedulers::constraint::scheduler TScheduler>
+template<constraint::decayed_type Type, constraint::observable_of_type<Type> TObs, schedulers::constraint::scheduler TScheduler>
 auto subscribe_on_impl(TObs&& obs, TScheduler&& scheduler);
 
 template<constraint::decayed_type Type, typename SpecificObservable>
 struct member_overload<Type, SpecificObservable, subscribe_on_tag>
 {
     /**
-    * \brief
+    * \brief forces observable to operate in provided scheduler (on_subscribe called via scheduling)
     *
-    * \details
-    *	
     * Example:
+    * \snippet subscribe_on.cpp subscribe_on
     *
-    * \see 
+    * \see https://reactivex.io/documentation/operators/subscribeon.html
     *
     * \return new specific_observable with the subscribe_on operator as most recent operator.
     * \warning #include <rpp/operators/subscribe_on.h>
     * \ingroup operators
     */
-    template<rpp::schedulers::constraint::scheduler TScheduler>
+    template<schedulers::constraint::scheduler TScheduler>
     auto subscribe_on(const TScheduler& scheduler) const & requires is_header_included<subscribe_on_tag, TScheduler>
     {
         return subscribe_on_impl<Type>(*static_cast<const SpecificObservable*>(this), scheduler);
     }
     
-    template<rpp::schedulers::constraint::scheduler TScheduler>
+    template<schedulers::constraint::scheduler TScheduler>
     auto subscribe_on(const TScheduler& scheduler) && requires is_header_included<subscribe_on_tag, TScheduler>
     {
         return subscribe_on_impl<Type>(std::move(*static_cast<SpecificObservable*>(this)), scheduler);
