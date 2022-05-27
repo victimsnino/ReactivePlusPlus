b5a1149f8463655e93c2a949451530bf969845e5
victimsnino <victimsnino@users.noreply.github.com>
2022-05-27 18:05:34 +0000

    deploy: 2c109e9ea9ce72713e7a5fe428eba2f2a28451c7

--- docs/html/search/all_e.js
+++ docs/html/search/all_e.js
@@ -3,5 +3,6 @@ var searchData=
   ['reactiveplusplus_0',['ReactivePlusPlus',['../index.html',1,'']]],
   ['ref_5fcount_1',['ref_count',['../group__operators.html#gac1991f003be32f5432ecb084be506c15',1,'rpp::details::member_overload&lt; Type, SpecificObservable, ref_count_tag &gt;']]],
   ['repeat_2',['repeat',['../group__operators.html#ga2a165810b188f9cf9b77255ed725e31a',1,'rpp::details::member_overload&lt; Type, SpecificObservable, repeat_tag &gt;::repeat(size_t count) const &amp;'],['../group__operators.html#ga152a343d0ea926ee04cf64ebb78c5d5e',1,'rpp::details::member_overload&lt; Type, SpecificObservable, repeat_tag &gt;::repeat() const &amp;']]],
-  ['repeat_5fon_5fcompleted_3',['repeat_on_completed',['../classrpp_1_1details_1_1repeat__on__completed.html',1,'rpp::details']]]
+  ['repeat_5fon_5fcompleted_3',['repeat_on_completed',['../classrpp_1_1details_1_1repeat__on__completed.html',1,'rpp::details']]],
+  ['run_5floop_4',['run_loop',['../classrpp_1_1schedulers_1_1run__loop.html',1,'rpp::schedulers']]]
 ];
--- docs/html/search/classes_e.js
+++ docs/html/search/classes_e.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['with_5flatest_5ffrom_5fstate_5ft_0',['with_latest_from_state_t',['../structrpp_1_1details_1_1with__latest__from__state__t.html',1,'rpp::details']]],
   ['worker_1',['worker',['../classrpp_1_1schedulers_1_1worker.html',1,'rpp::schedulers']]],
-  ['worker_5fstrategy_2',['worker_strategy',['../classrpp_1_1schedulers_1_1immediate_1_1worker__strategy.html',1,'rpp::schedulers::immediate::worker_strategy'],['../classrpp_1_1schedulers_1_1new__thread_1_1worker__strategy.html',1,'rpp::schedulers::new_thread::worker_strategy']]]
+  ['worker_5fstrategy_2',['worker_strategy',['../classrpp_1_1schedulers_1_1immediate_1_1worker__strategy.html',1,'rpp::schedulers::immediate::worker_strategy'],['../classrpp_1_1schedulers_1_1new__thread_1_1worker__strategy.html',1,'rpp::schedulers::new_thread::worker_strategy'],['../classrpp_1_1schedulers_1_1run__loop_1_1worker__strategy.html',1,'rpp::schedulers::run_loop::worker_strategy']]]
 ];
