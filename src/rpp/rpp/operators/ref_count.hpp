05b852740cbb628b3ed89a8683ccc556f88bdbd8
Aleksey Loginov <victimsnino@gmail.com>
2022-06-13 23:22:06 +0300

    simplify ref_count

--- src/rpp/rpp/operators/ref_count.hpp
+++ src/rpp/rpp/operators/ref_count.hpp
@@ -19,36 +19,37 @@ IMPLEMENTATION_FILE(ref_count_tag);
 
 namespace rpp::details
 {
+struct ref_count_state_t
+{
+    bool on_subscribe()
+    {
+        std::lock_guard lock{m_mutex};
+        if (++m_count_of_active_subs != 1)
+            return false;
+
+        m_sub = composite_subscription{};
+        return true;
+    }
+
+    void on_unsubscribe()
+    {
+        std::lock_guard lock{ m_mutex };
+        if (--m_count_of_active_subs == 0)
+            m_sub.unsubscribe();
+    }
+
+    const composite_subscription& get_subscription() const { return m_sub; }
+
+private:
+    size_t                 m_count_of_active_subs{};
+    composite_subscription m_sub = composite_subscription::empty();
+    std::mutex             m_mutex{};
+};
+
 template<constraint::decayed_type Type, constraint::observable_of_type<Type> TObs>
 auto ref_count_impl(TObs&& observable)
 {
-    struct state_t
-    {
-        bool on_subscribe()
-        {
-            std::lock_guard lock{m_mutex};
-            if (++m_count_of_active_subs != 1)
-                return false;
-
-            m_sub = composite_subscription{};
-            return true;
-        }
-
-        void on_unsubscribe()
-        {
-            std::lock_guard lock{ m_mutex };
-            if (--m_count_of_active_subs == 0)
-                m_sub.unsubscribe();
-        }
-
-        const composite_subscription& get_subscription() const { return m_sub; }
-
-    private:
-        size_t                 m_count_of_active_subs{};
-        composite_subscription m_sub = composite_subscription::empty();
-        std::mutex             m_mutex{};
-    };
-    return source::create<Type>([observable = std::forward<TObs>(observable), state = std::make_shared<state_t>()](const constraint::subscriber_of_type<Type> auto& subscriber)
+    return source::create<Type>([observable = std::forward<TObs>(observable), state = std::make_shared<ref_count_state_t>()](const constraint::subscriber_of_type<Type> auto& subscriber)
     {
         const bool need_to_connect = state->on_subscribe();
 
