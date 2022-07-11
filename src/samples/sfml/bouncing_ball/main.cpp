c8fcb660e54bcc4ffa228f52017b70534af7a582
Aleksey Loginov <victimsnino@gmail.com>
2022-07-11 15:20:37 +0300

    Modify a bit

--- src/samples/sfml/bouncing_ball/main.cpp
+++ src/samples/sfml/bouncing_ball/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <SFML/Graphics.hpp>
+#include <rpp/rpp.hpp>
 
 #include <filesystem>
 
@@ -58,20 +59,118 @@ auto prepare_text_shape(const sf::Font& font)
     return text;
 }
 
+uint8_t deduce_color_smoothly(size_t color_value)
+{
+    const uint8_t rest = color_value % 255;
+    const uint32_t overflow_count = color_value / 255;
+    if (overflow_count % 2)
+        return uint8_t{255} - rest;
+    return rest;
+}
+
+struct PresentEvent
+{
+    bool is_begin{};
+    size_t frame_number{};
+};
+
+using CustomEvent = std::variant<PresentEvent, sf::Event>;
+
+auto get_events_observable(sf::RenderWindow& window)
+{
+    return rpp::source::create<CustomEvent>([&window](const auto& sub)
+    {
+        sf::Event ev{};
+
+        size_t frame_number{};
+        while (window.isOpen() && sub.is_subscribed())
+        {
+            sub.on_next(PresentEvent{true, frame_number});
+
+            while (window.pollEvent(ev))
+                sub.on_next(ev);
+
+            sub.on_next(PresentEvent{false, frame_number++});
+        }
+    });
+}
+
+template<typename State>
+void handle_presents(const rpp::specific_observable<CustomEvent, State>& events_observable, sf::RenderWindow& window)
+{
+    auto presents = events_observable.filter([](const CustomEvent& ev)
+                                     {
+                                         return std::holds_alternative<PresentEvent>(ev);
+                                     })
+                                     .map([](const CustomEvent& ev)
+                                     {
+                                         return std::get<PresentEvent>(ev);
+                                     });
+
+    presents.filter([](const PresentEvent& ev)
+            {
+                return ev.is_begin;
+            })
+            .subscribe([&window](const auto& ev)
+            {
+                window.clear(sf::Color{deduce_color_smoothly(ev.frame_number * 1), 
+                                       deduce_color_smoothly(ev.frame_number * 2),
+                                       deduce_color_smoothly(ev.frame_number * 3)});
+            });
+
+    presents.filter([](const PresentEvent& ev)
+            {
+                return !ev.is_begin;
+            })
+            .subscribe([&window](const auto&)
+            {
+                window.display();
+            });
+}
+
+template<typename State>
+void handle_ball_position(const rpp::specific_observable<CustomEvent, State>& events_observable)
+{
+    auto end_presents = events_observable
+                    .filter([](const CustomEvent& ev)
+                    {
+                        return std::holds_alternative<PresentEvent>(ev);
+                    })
+                    .filter([](const CustomEvent& ev)
+                    {
+                        return std::get<PresentEvent>(ev).is_begin == false;
+                    });
+}
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(sf::Vector2u{680, 680}), "Bouncing Ball");
 
-    auto font = load_font();
+    auto font       = load_font();
     auto ball_shape = prepare_ball_shape();
     auto text_shape = prepare_text_shape(font);
-    
-    float x = 50;
-    float y = 150;
+
+    float x       = 50;
+    float y       = 150;
     float x_speed = 1.f;
     float y_speed = 1.f;
- 
-    while (window.isOpen())
+
+    auto events = get_events_observable(window).publish();
+
+    // clear on begin, display on end
+    handle_presents(events, window);
+
+    // this one will be blocking call and it will unblock when close requested
+    events.ref_count()
+          .filter([](const CustomEvent& ev) { return std::holds_alternative<sf::Event>(ev); })
+          .filter([](const CustomEvent& ev) { return std::get<sf::Event>(ev).type == sf::Event::Closed; })
+          .take(1)
+          .subscribe([&](const auto&)
+          {
+              window.close();
+          });
+    
+   /* while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
@@ -100,7 +199,7 @@ int main()
         window.draw(ball_shape);
         window.draw(text_shape);
         window.display();
-    }
+    }*/
  
     return EXIT_SUCCESS;
 }
