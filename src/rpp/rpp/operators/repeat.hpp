76d283dc3c4a86b4bee580d77a4ac6e21dd22ade
Aleksey Loginov <victimsnino@gmail.com>
2022-07-29 13:01:06 +0300

    Speedup repeat

--- src/rpp/rpp/operators/repeat.hpp
+++ src/rpp/rpp/operators/repeat.hpp
@@ -22,19 +22,22 @@ namespace rpp::details
 template<constraint::decayed_type Type, typename SpecificObservable, typename Predicate>
 class repeat_on_completed
 {
-public:
-    repeat_on_completed(std::shared_ptr<SpecificObservable> shared_observable,
-                        Predicate                           predicate)
-        : m_shared_observable{std::move(shared_observable)}
-        , m_predicate{std::move(predicate)} {}
+    struct state_t
+    {
+        SpecificObservable              observable;
+        [[no_unique_address]] Predicate predicate;
+    };
 
-    repeat_on_completed(const repeat_on_completed<Type, SpecificObservable, Predicate>&) = default;
+public:
+    repeat_on_completed(const SpecificObservable& observable, Predicate&& predicate)
+        : m_state{std::make_shared<state_t>(observable, std::move(predicate))} {}
+
 
     void operator()(const auto& sub) const
     {
         if (sub.is_subscribed())
         {
-            if (m_predicate())
+            if (m_state->predicate())
                 subscribe_subscriber_for_repeat(sub);
             else
                 sub.on_completed();
@@ -44,48 +47,46 @@ public:
 private:
     void subscribe_subscriber_for_repeat(const constraint::subscriber auto& subscriber) const
     {
-        m_shared_observable->subscribe(create_subscriber_with_state<Type>(subscriber.get_subscription().make_child(),
+        m_state->observable.subscribe(create_subscriber_with_state<Type>(subscriber.get_subscription().make_child(),
                                                                           subscriber,
                                                                           forwarding_on_next{},
                                                                           forwarding_on_error{},
                                                                           *this));
     }
 
-    std::shared_ptr<SpecificObservable> m_shared_observable;
-    [[no_unique_address]] Predicate     m_predicate;
+    std::shared_ptr<state_t> m_state;
 };
 
 struct counted_repeat_predicate
 {
     counted_repeat_predicate(size_t count)
-        : m_count{std::make_shared<size_t>(count)} {}
+        : m_count{count} {}
 
-    bool operator()() const { return *m_count && (*m_count)--; }
+    bool operator()() { return m_count && m_count--; }
 private:
-    std::shared_ptr<size_t> m_count{};
+    size_t m_count{};
 };
 
 template<constraint::decayed_type Type, constraint::observable_of_type<Type> TObs, typename CreatePredicateFn>
 struct repeat_on_subscribe
 {
     repeat_on_subscribe(TObs&& observable, CreatePredicateFn&& create_predicate)
-        : m_shared_observable{std::make_shared<TObs>(std::move(observable))}
+        : m_observable{std::move(observable)}
         , m_create_predicate{std::move(create_predicate)} {}
 
     repeat_on_subscribe(const TObs& observable, CreatePredicateFn&& create_predicate)
-        : m_shared_observable{ std::make_shared<TObs>(observable) }
-        , m_create_predicate{ std::move(create_predicate) } {}
+        : m_observable{observable}
+        , m_create_predicate{std::move(create_predicate)} {}
 
     template<constraint::subscriber_of_type<Type> TSub>
     void operator()(const TSub& subscriber) const
     {
-        auto predicate = m_create_predicate();
-        repeat_on_completed<Type, std::decay_t<TObs>, decltype(predicate)>{m_shared_observable, std::move(predicate)}(subscriber);
+        repeat_on_completed<Type, std::decay_t<TObs>, std::invoke_result_t<CreatePredicateFn>>{m_observable, m_create_predicate()}(subscriber);
     }
 
 private:
-    std::shared_ptr<TObs> m_shared_observable{};
-    CreatePredicateFn     m_create_predicate;
+    TObs              m_observable;
+    CreatePredicateFn m_create_predicate;
 };
 
 template<constraint::decayed_type Type, constraint::observable_of_type<Type> TObs, typename CreatePredicateFn>
