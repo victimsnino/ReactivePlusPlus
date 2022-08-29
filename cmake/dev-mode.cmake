c63454c105cb6be574610c2edad13d02d1edb646
Aleksey Loginov <victimsnino@gmail.com>
2022-08-29 17:34:58 +0300

    fix

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -4,3 +4,7 @@
 
 # include(cmake/lint-targets.cmake)
 # include(cmake/spell-targets.cmake)
+
+if(CMAKE_EXPORT_COMPILE_COMMANDS)
+    set(CMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES ${CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES})
+endif()
