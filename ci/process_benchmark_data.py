62a18a2be4e1fbe86eac1a3db6d4d343d6498e4c
Aleksey Loginov <victimsnino@gmail.com>
2022-08-31 23:50:03 +0300

    fix process_benchmark_data

--- ci/process_benchmark_data.py
+++ ci/process_benchmark_data.py
@@ -11,7 +11,10 @@ new_results = pd.DataFrame()
 for file in os.listdir(os.fsencode("./artifacts")):
     folder_with_results = os.fsdecode(file)
     for source in ['rpp', 'rxcpp']:
-        new_data = parse(f'./artifacts/{folder_with_results}/{source}_benchmark_result.txt')
+        file_name = f'./artifacts/{folder_with_results}/{source}_benchmark_result.txt'
+        if not os.path.exists(file_name):
+            continue
+        new_data = parse(file_name)
         new_data["platform"] = [folder_with_results]*len(new_data)
         new_data["commit"] = [f"{commit_message[:20]}{'...' if len(commit_message)> 20 else ''} ({git_commit})"]*len(new_data)
         new_data["source"] = [source]*len(new_data)
