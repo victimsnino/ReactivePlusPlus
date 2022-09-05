f295ba262df2ed17a849617a41a93b1a3f9a784c
TC Wang <boyw165@gmail.com>
2022-09-05 15:52:00 -0700

    Add race-condition test for "take_until" operator.

--- src/tests/test_take_until.cpp
+++ src/tests/test_take_until.cpp
@@ -11,6 +11,8 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <rpp/operators/take_until.hpp>
+#include <rpp/schedulers/new_thread_scheduler.hpp>
+#include <rpp/schedulers/trampoline_scheduler.hpp>
 #include <rpp/sources/error.hpp>
 #include <rpp/sources/interval.hpp>
 #include <rpp/sources/just.hpp>
@@ -91,5 +93,29 @@ SCENARIO("take_until mirrors both source observable and trigger observable", "[t
             CHECK(mock.get_on_completed_count() == 0);
             CHECK(mock.get_on_error_count() == 1);
         }
+    }
+}
+
+SCENARIO("take_until can handle race condition")
+{
+    GIVEN("source observable and trigger observable emits in different thread")
+    {
+        std::atomic_bool on_completed_called{false};
+
+        rpp::source::interval(std::chrono::seconds{1}, rpp::schedulers::trampoline{})
+            .take_until(rpp::source::interval(std::chrono::seconds{2}, rpp::schedulers::new_thread{}))
+            .subscribe([&](const auto&)
+                       {
+                           CHECK(!on_completed_called);
+                           std::this_thread::sleep_for(std::chrono::seconds(3));
+                           CHECK(!on_completed_called);
+                       } /* on_next */,
+                       {} /* on_error */,
+                       [&]()
+                       {
+                           on_completed_called = true;
+                       } /* on_completed */);
+
+        CHECK(on_completed_called);
     }
 }
