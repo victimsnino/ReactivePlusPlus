f10b77948d248387dc26ab1d15df51ff6c677c70
Aleksey Loginov <victimsnino@gmail.com>
2022-09-06 23:07:47 +0300

    switch_on_next

--- src/rpp/rpp/operators/switch_on_next.hpp
+++ src/rpp/rpp/operators/switch_on_next.hpp
@@ -54,7 +54,7 @@ struct switch_on_next_on_next
         using ValueType = utils::extract_observable_type_t<TObs>;
 
         state->current_inner_observable.unsubscribe();
-        state->current_inner_observable = sub.get_subscription().make_child();
+        state->current_inner_observable = state->childs_subscriptions.make_child();
         state->current_inner_observable.add([state = std::weak_ptr{state}]
         {
             if (const auto locked = state.lock())
@@ -82,11 +82,11 @@ struct switch_on_next_impl
     template<constraint::subscriber_of_type<ValueType> TSub>
     auto operator()(TSub&& subscriber) const
     {
-        auto state = std::make_shared<switch_on_next_state>();
+        auto state = std::make_shared<switch_on_next_state>(subscriber.get_subscription());
 
         state->count_of_on_completed_needed.fetch_add(1, std::memory_order::relaxed);
 
-        auto subscription = subscriber.get_subscription().make_child();
+        auto subscription = state->childs_subscriptions.make_child();
         return create_subscriber_with_state<Type>(std::move(subscription),
                                                   switch_on_next_on_next{},
                                                   switch_on_next_on_error{},
