bdc77c49531698d642b34b03c9e29bc6b06514dd
Aleksey Loginov <victimsnino@gmail.com>
2022-09-06 13:53:53 +0300

    fix concat

--- src/rpp/rpp/operators/merge.hpp
+++ src/rpp/rpp/operators/merge.hpp
@@ -36,7 +36,7 @@ struct merge_forwarding_on_next
 {
     void operator()(auto&&                              value,
                     const constraint::subscriber auto&  sub,
-                    const std::shared_ptr<merge_state>& state) const
+                    const auto& state) const
     {
         std::lock_guard lock{state->mutex};
         sub.on_next(std::forward<decltype(value)>(value));
