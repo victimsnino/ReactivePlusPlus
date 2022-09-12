e8cc6a60784d5523afbffa2a313ffcbf932037ce
TC Wang <boyw165@gmail.com>
2022-09-11 23:20:53 -0700

    nit: Sort imports.

--- src/rpp/rpp/operators/observe_on.hpp
+++ src/rpp/rpp/operators/observe_on.hpp
@@ -12,13 +12,12 @@
 
 #include <rpp/defs.hpp>
 #include <rpp/operators/delay.hpp>
-#include <rpp/operators/details/subscriber_with_state.hpp> // create_subscriber_with_state
 #include <rpp/operators/fwd/observe_on.hpp>
 #include <rpp/subscribers/constraints.hpp>
 
+#include <rpp/operators/details/subscriber_with_state.hpp> // create_subscriber_with_state
 
 IMPLEMENTATION_FILE(observe_on_tag);
-
 
 namespace rpp::details
 {
