8f6f384a75df789b5bccd67c262356ad39f3ea6a
Aleksey Loginov <victimsnino@gmail.com>
2022-09-12 23:10:07 +0300

    polish observe-on

--- src/rpp/rpp/operators/observe_on.hpp
+++ src/rpp/rpp/operators/observe_on.hpp
@@ -10,40 +10,9 @@
 
 #pragma once
 
-#include <rpp/defs.hpp>
-#include <rpp/operators/delay.hpp>
 #include <rpp/operators/fwd/observe_on.hpp>
-#include <rpp/subscribers/constraints.hpp>
-
-#include <rpp/operators/details/subscriber_with_state.hpp> // create_subscriber_with_state
 
 IMPLEMENTATION_FILE(observe_on_tag);
 
-namespace rpp::details
-{
-using observe_on_on_next = delay_on_next;
-using observe_on_on_error = delay_on_error;
-using observe_on_on_completed = delay_on_completed;
-
-template<constraint::decayed_type Type, schedulers::constraint::scheduler TScheduler>
-struct observe_on_impl
-{
-    RPP_NO_UNIQUE_ADDRESS TScheduler scheduler;
-
-    template<constraint::subscriber_of_type<Type> TSub>
-    auto operator()(TSub&& subscriber) const
-    {
-        // convert it to dynamic due to expected amount of copies == amount of items
-        auto dynamic_subscriber = std::forward<TSub>(subscriber).as_dynamic();
-
-        auto worker = scheduler.create_worker(dynamic_subscriber.get_subscription());
-
-        return create_subscriber_with_state<Type>(dynamic_subscriber.get_subscription().make_child(),
-                                                  observe_on_on_next{rpp::schedulers::duration{0}},
-                                                  observe_on_on_error{rpp::schedulers::duration{0}},
-                                                  observe_on_on_completed{rpp::schedulers::duration{0}},
-                                                  std::move(dynamic_subscriber),
-                                                  std::move(worker));
-    }
-};
-} // namespace rpp::details
+// we just need delay to completed this one
+#include <rpp/operators/delay.hpp>
