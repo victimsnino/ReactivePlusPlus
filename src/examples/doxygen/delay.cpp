a8302287210e679402aac8692a45d0e5a380bd33
Aleksey Loginov <victimsnino@gmail.com>
2022-09-12 23:17:50 +0300

    fix

--- src/examples/doxygen/delay.cpp
+++ src/examples/doxygen/delay.cpp
@@ -17,14 +17,14 @@ int main()
             .do_on_next([&](auto&& v)
                         {
                             auto emitting_time = rpp::schedulers::clock_type::now();
-                            std::cout << "emit " << v << " in thread{" << std::this_thread::get_id() << "} duration since start " << std::chrono::duration_cast<std::chrono::seconds>(emitting_time - start) << std::endl;
+                            std::cout << "emit " << v << " in thread{" << std::this_thread::get_id() << "} duration since start " << std::chrono::duration_cast<std::chrono::seconds>(emitting_time - start).count() << "s"<< std::endl;
                         })
             .delay(std::chrono::seconds{3}, rpp::schedulers::new_thread{})
             .as_blocking()
             .subscribe([&](int v)
                        {
                            auto observing_time = rpp::schedulers::clock_type::now();
-                           std::cout << "observe " << v << " in thread{" << std::this_thread::get_id() << "} duration since start " << std::chrono::duration_cast<std::chrono::seconds>(observing_time - start) << std::endl;
+                           std::cout << "observe " << v << " in thread{" << std::this_thread::get_id() << "} duration since start " << std::chrono::duration_cast<std::chrono::seconds>(observing_time - start).count() <<"s" << std::endl;
                        });
     // Template for output:
     //    emit 1 in thread{11772} duration since start 0s
