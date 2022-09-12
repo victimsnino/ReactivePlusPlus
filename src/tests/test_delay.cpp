3f9f9d909e644b3d0c19e5de712a16829be88833
Aleksey Loginov <victimsnino@gmail.com>
2022-09-12 23:12:18 +0300

    Polish tests

--- src/tests/test_delay.cpp
+++ src/tests/test_delay.cpp
@@ -8,15 +8,14 @@
 //
 //  Project home: https://github.com/victimsnino/ReactivePlusPlus
 
-#include <catch2/catch_test_macros.hpp>
+#include "mock_observer.hpp"
 
+#include <catch2/catch_test_macros.hpp>
 #include <rpp/operators/delay.hpp>
 #include <rpp/schedulers/trampoline_scheduler.hpp>
 #include <rpp/sources/empty.hpp>
 #include <rpp/sources/error.hpp>
 #include <rpp/sources/just.hpp>
-
-#include "mock_observer.hpp"
 
 SCENARIO("delay mirrors both source observable and trigger observable", "[delay]")
 {
@@ -24,31 +23,32 @@ SCENARIO("delay mirrors both source observable and trigger observable", "[delay]
 
     GIVEN("observable of -1-|")
     {
-        auto mock = mock_observer<int>{};
-        const auto now = rpp::schedulers::clock_type::now();
+        auto       mock = mock_observer<int>{};
+        const auto now  = rpp::schedulers::clock_type::now();
 
         rpp::source::just(1)
-            .delay(delay_duration, rpp::schedulers::trampoline{})
-            .as_blocking()
-            .subscribe([&](auto&& v)
-                       {
-                           THEN("should see event after the delay")
+                .delay(delay_duration, rpp::schedulers::trampoline{})
+                .as_blocking()
+                .subscribe(
+                           [&](auto&& v)
                            {
-                               CHECK(rpp::schedulers::clock_type::now() >= now + std::chrono::duration_cast<rpp::schedulers::duration>(delay_duration));
-                           }
+                               THEN("should see event after the delay")
+                               {
+                                   CHECK(rpp::schedulers::clock_type::now() >= now + delay_duration);
+                               }
 
-                           mock.on_next(v);
-                       },
-                       [&](const std::exception_ptr& err) { mock.on_error(err); },
-                       [&]()
-                       {
-                           THEN("should see event after the delay")
+                               mock.on_next(v);
+                           },
+                           [&](const std::exception_ptr& err) { mock.on_error(err); },
+                           [&]()
                            {
-                               CHECK(rpp::schedulers::clock_type::now() >= now + std::chrono::duration_cast<rpp::schedulers::duration>(delay_duration));
-                           }
+                               THEN("should see event after the delay")
+                               {
+                                   CHECK(rpp::schedulers::clock_type::now() >= now + delay_duration);
+                               }
 
-                           mock.on_completed();
-                       });
+                               mock.on_completed();
+                           });
 
         THEN("should see -1-|")
         {
@@ -60,22 +60,22 @@ SCENARIO("delay mirrors both source observable and trigger observable", "[delay]
 
     GIVEN("observable of -x")
     {
-        auto mock = mock_observer<int>{};
-        const auto now = rpp::schedulers::clock_type::now();
+        auto       mock = mock_observer<int>{};
+        const auto now  = rpp::schedulers::clock_type::now();
 
         rpp::source::error<int>(std::make_exception_ptr(std::runtime_error{""}))
-            .delay(delay_duration, rpp::schedulers::trampoline{})
-            .as_blocking()
-            .subscribe([&](auto&& v) { mock.on_next(v); },
-                       [&](const std::exception_ptr& err)
-                       {
-                           THEN("should see event after the delay")
+                .delay(delay_duration, rpp::schedulers::trampoline{})
+                .as_blocking()
+                .subscribe([&](auto&&                    v) { mock.on_next(v); },
+                           [&](const std::exception_ptr& err)
                            {
-                               CHECK(rpp::schedulers::clock_type::now() >= now + std::chrono::duration_cast<rpp::schedulers::duration>(delay_duration));
-                           }
-                           mock.on_error(err);
-                       },
-                       [&]() { mock.on_completed(); });
+                               THEN("should see event immediately")
+                               {
+                                   CHECK(rpp::schedulers::clock_type::now() < now + delay_duration);
+                               }
+                               mock.on_error(err);
+                           },
+                           [&]() { mock.on_completed(); });
 
         THEN("should see -x after the delay")
         {
@@ -87,22 +87,22 @@ SCENARIO("delay mirrors both source observable and trigger observable", "[delay]
 
     GIVEN("observable of -|")
     {
-        auto mock = mock_observer<int>{};
-        const auto now = rpp::schedulers::clock_type::now();
+        auto       mock = mock_observer<int>{};
+        const auto now  = rpp::schedulers::clock_type::now();
 
         rpp::source::empty<int>()
-            .delay(delay_duration, rpp::schedulers::trampoline{})
-            .as_blocking()
-            .subscribe([&](auto&& v) { mock.on_next(v); },
-                       [&](const std::exception_ptr& err) { mock.on_error(err); },
-                       [&]()
-                       {
-                           THEN("should see event after delay")
+                .delay(delay_duration, rpp::schedulers::trampoline{})
+                .as_blocking()
+                .subscribe([&](auto&&                    v) { mock.on_next(v); },
+                           [&](const std::exception_ptr& err) { mock.on_error(err); },
+                           [&]()
                            {
-                              CHECK(rpp::schedulers::clock_type::now() >= now + std::chrono::duration_cast<rpp::schedulers::duration>(delay_duration));
-                           }
-                           mock.on_completed();
-                       });
+                               THEN("should see event after delay")
+                               {
+                                   CHECK(rpp::schedulers::clock_type::now() >= now + delay_duration);
+                               }
+                               mock.on_completed();
+                           });
 
         THEN("should see -|")
         {
