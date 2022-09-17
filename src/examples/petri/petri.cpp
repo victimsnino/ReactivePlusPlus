b2d088030bb5f15d6ee520daafef661a23a21bfe
Thomas Horstink <thomashorstink@gmail.com>
2022-09-17 21:18:17 +0200

    update names to be clear and in line with project code style

--- src/examples/petri/petri.cpp
+++ src/examples/petri/petri.cpp
@@ -25,35 +25,35 @@ void foo() { std::cout << "foo" << std::endl; }
 void bar() { std::cout << "bar" << std::endl; }
 
 // The state of a system is determined by its Petri Net (constant) and its marking (changes)
-const PetriNet net     = {{0, {{'a', 'b'}, {'c'}}}, {1, {{'c', 'c'}, {'b', 'b', 'd'}}}};
-const Marking  m_start = {{'a', 4}, {'b', 2}, {'c', 0}, {'d', 0}};
+const PetriNet net = {{0, {{'a', 'b'}, {'c'}}}, {1, {{'c', 'c'}, {'b', 'b', 'd'}}}};
+const Marking  marking_start = {{'a', 4}, {'b', 2}, {'c', 0}, {'d', 0}};
 
 const std::map<Transition, TransitionFunction> store = {{0, &foo}, {1, &bar}};
 
-const auto& runTransition(Transition t)
+const auto& run_transition(Transition t)
 {
     store.at(t)();            // calls the function associated with the transition
     return net.at(t).produce; // contains the post marking mutations.
 }
 
-std::pair<Marking, Transitions> mutateMarking(Marking&& m, const Places& produce)
+std::pair<Marking, Transitions> mutate_marking(Marking&& marking, const Places& produce)
 {
     // Produce tokens
-    for_each(produce, [&m](const auto& place) { m[place] += 1; });
+    for_each(produce, [&marking](const auto& place) { marking[place] += 1; });
     // Consume tokens by exeucting enabled transitions
     Transitions transitions;
     for (const auto& [transition, mutations] : net)
     {
         const auto& c = mutations.consume;
         if (!c.empty() &&
-            all_of(c, [&](const auto& place) { return m[place] >= count(c, place); }))
+            all_of(c, [&](const auto& place) { return marking[place] >= count(c, place); }))
         {
-            for_each(c, [&m](const auto& place) { m[place] -= 1; });
+            for_each(c, [&marking](const auto& place) { marking[place] -= 1; });
             transitions.push_back(transition);
         }
     }
 
-    return std::make_pair(m, transitions);
+    return std::make_pair(marking, transitions);
 }
 
 int main()
@@ -64,24 +64,24 @@ int main()
     // In our case, we have a finite program and we expect the program to end up in a
     // particular marking. Hence we create a predicate that compares the marking to the
     // expected (end) marking.
-    const Marking m_end         = {{'a', 0}, {'b', 2}, {'c', 0}, {'d', 2}};
-    const auto    end_predicate = [=](const auto& m)
-    { return !std::equal(m.begin(), m.end(), m_end.begin()); };
+    const Marking marking_end   = {{'a', 0}, {'b', 2}, {'c', 0}, {'d', 2}};
+    const auto    end_predicate = [=](const auto& marking)
+    { return !std::equal(marking.begin(), marking.end(), marking_end.begin()); };
 
     // Reactive streams lend themselves very nicely to create a 'petri net executor':
     transitions_subject.get_observable()
-        .map(&runTransition)
+        .map(&run_transition)
         .start_with(Places{}) // we start with an empty set
         .observe_on(rpp::schedulers::trampoline{})
-        .scan(m_start,
-              [&transitions_subject](auto&& m, const auto& p)
+        .scan(marking_start,
+              [&transitions_subject](auto&& marking, const auto& places)
               {
-                  Transitions new_t;
-                  std::tie(m, new_t) = mutateMarking(std::move(m), p);
-                  for_each(new_t,
+                  Transitions transitions;
+                  std::tie(marking, transitions) = mutate_marking(std::move(marking), places);
+                  for_each(transitions,
                            [dispatcher = transitions_subject.get_subscriber()](const auto& t)
                            { dispatcher.on_next(t); });
-                  return m;
+                  return marking;
               })
         .take_while(end_predicate)
         .subscribe([](const auto&) {}, [] { std::cout << "done" << std::endl; });
