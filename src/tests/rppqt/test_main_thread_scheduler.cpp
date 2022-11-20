a7e0e8ef2c7f2f7020c817924c1c100bbe05bd60
Aleksey Loginov <victimsnino@gmail.com>
2022-11-20 22:50:02 +0300

    Extend tests

--- src/tests/rppqt/test_main_thread_scheduler.cpp
+++ src/tests/rppqt/test_main_thread_scheduler.cpp
@@ -8,6 +8,7 @@
 //  Project home: https://github.com/victimsnino/ReactivePlusPlus
 
 #include <catch2/catch_test_macros.hpp>
+#include <rpp/subscriptions/composite_subscription.hpp>
 
 #include <rppqt/schedulers/main_thread_scheduler.hpp>
 
@@ -28,16 +29,37 @@ SCENARIO("main_thread_scheduler schedules actions to main thread", "[schedulers]
                 rppqt::schedulers::main_thread_scheduler{}.create_worker().schedule([&]()->rpp::schedulers::optional_duration
                 {
                     execution_thread.set_value(std::this_thread::get_id());
-                    application.exit();
                     return {};
                 });
             }}.join();
+            QTimer::singleShot(10, &application, [&]{application.exit();});
             application.exec();
             THEN("thread of exectuion of schedulable should be same as thread of application")
             {
                 auto future = execution_thread.get_future();
                 REQUIRE(future.wait_for(std::chrono::seconds{3}) == std::future_status::ready);
                 CHECK(future.get() == std::this_thread::get_id());
+            }
+        }
+        WHEN("submitting action to main scheduler from another thread but with unsubscribed subscription after schedule")
+        {
+            std::promise<std::thread::id> execution_thread{};
+            std::thread{[&]
+            {
+                rpp::composite_subscription sub{};
+                rppqt::schedulers::main_thread_scheduler{}.create_worker(sub).schedule([&]()->rpp::schedulers::optional_duration
+                {
+                    execution_thread.set_value(std::this_thread::get_id());
+                    return {};
+                });
+                sub.unsubscribe();
+            }}.join();
+            QTimer::singleShot(10, &application, [&]{application.exit();});
+            application.exec();
+            THEN("no schedule execution")
+            {
+                auto future = execution_thread.get_future();
+                REQUIRE(future.wait_for(std::chrono::seconds{1}) == std::future_status::timeout);
             }
         }
     }
