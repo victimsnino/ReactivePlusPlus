84e72f7f0fc011eae728f2611532d2962728d8ff
Aleksey Loginov <victimsnino@gmail.com>
2022-11-20 22:46:26 +0300

    Add tests

--- src/tests/rppqt/test_main_thread_scheduler.cpp
+++ src/tests/rppqt/test_main_thread_scheduler.cpp
@@ -8,10 +8,38 @@
 //  Project home: https://github.com/victimsnino/ReactivePlusPlus
 
 #include <catch2/catch_test_macros.hpp>
+#include "mock_observer.hpp"
 
 #include <rppqt/schedulers/main_thread_scheduler.hpp>
 
+#include <future>
+#include <QApplication>
+
 SCENARIO("main_thread_scheduler schedules actions to main thread", "[schedulers]")
 {
-    
+    GIVEN("qapplication and scheduler")
+    {
+        int argc{};
+        QApplication application{argc, nullptr};
+        WHEN("submitting action to main scheduler from another thread")
+        {
+            std::promise<std::thread::id> execution_thread{};
+            std::thread{[&]
+            {
+                rppqt::schedulers::main_thread_scheduler{}.create_worker().schedule([&]()->rpp::schedulers::optional_duration
+                {
+                    execution_thread.set_value(std::this_thread::get_id());
+                    application.exit();
+                    return {};
+                });
+            }}.join();
+            application.exec();
+            THEN("thread of exectuion of schedulable should be same as thread of application")
+            {
+                auto future = execution_thread.get_future();
+                REQUIRE(future.wait_for(std::chrono::seconds{3}) == std::future_status::ready);
+                CHECK(future.get() == std::this_thread::get_id());
+            }
+        }
+    }
 }
