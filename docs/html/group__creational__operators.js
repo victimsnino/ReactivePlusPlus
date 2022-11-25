0eb1cf4acef4b46686f33b393bf79987e7a76ce3
victimsnino <victimsnino@users.noreply.github.com>
2022-11-25 21:05:05 +0000

    deploy: 8b73c31183b9a919cd8284431fbb22de6aec8417

--- docs/html/group__creational__operators.js
+++ docs/html/group__creational__operators.js
@@ -4,7 +4,7 @@ var group__creational__operators =
     [ "rpp::observable::empty", "group__creational__operators.html#ga17c9442252280248e5c1e616fa7563df", null ],
     [ "rpp::observable::error", "group__creational__operators.html#gab2d1e859439b815edd56809381d204f3", null ],
     [ "rpp::observable::from_callable", "group__creational__operators.html#ga52959b5368cc881f38a305a3b5e1ded3", null ],
-    [ "rpp::observable::from_iterable", "group__creational__operators.html#gaa10b9e881db7e4a1ba5dad86d48e5220", null ],
+    [ "rpp::observable::from_iterable", "group__creational__operators.html#gaf796daac1f2e2450818dc95230946f54", null ],
     [ "rpp::observable::interval", "group__creational__operators.html#gace4fe134b05b9c02142166769df71436", null ],
     [ "rpp::observable::interval", "group__creational__operators.html#ga2c3002a4f3af8fcd0f73fab9bf5a0ea3", null ],
     [ "rpp::observable::just", "group__creational__operators.html#gab375dd4234133e4a10018cac93795f2f", null ],
--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -4,6 +4,6 @@ var searchData=
   ['first_1',['first',['../group__filtering__operators.html#ga9a3df2b8a6aff25e6283b3c9f9e8e1b0',1,'observable']]],
   ['flat_5fmap_2',['flat_map',['../group__transforming__operators.html#ga855aea700aa9aa9fa07e8aab7e193a1a',1,'observable']]],
   ['from_5fcallable_3',['from_callable',['../group__creational__operators.html#ga52959b5368cc881f38a305a3b5e1ded3',1,'rpp::observable']]],
-  ['from_5fiterable_4',['from_iterable',['../group__creational__operators.html#gaa10b9e881db7e4a1ba5dad86d48e5220',1,'rpp::observable::from_iterable(std::ranges::range auto &amp;&amp;iterable, const TScheduler &amp;scheduler)'],['../group__creational__operators.html#gaa10b9e881db7e4a1ba5dad86d48e5220',1,'rpp::observable::from_iterable(std::ranges::range auto &amp;&amp;iterable, const TScheduler &amp;scheduler=TScheduler{})']]],
+  ['from_5fiterable_4',['from_iterable',['../group__creational__operators.html#gaf796daac1f2e2450818dc95230946f54',1,'rpp::observable::from_iterable(constraint::iterable auto &amp;&amp;iterable, const TScheduler &amp;scheduler)'],['../group__creational__operators.html#gaf796daac1f2e2450818dc95230946f54',1,'rpp::observable::from_iterable(constraint::iterable auto &amp;&amp;iterable, const TScheduler &amp;scheduler=TScheduler{})']]],
   ['from_5fsignal_5',['from_signal',['../group__qt__creational__operators.html#ga4e3140c3f32d2ea4b80b76fdc772278b',1,'rppqt::observable']]]
 ];
