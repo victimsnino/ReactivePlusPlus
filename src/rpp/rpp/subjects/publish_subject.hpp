e2540f2209b6a0132f4d1b130413bef9506567a6
Aleksey Loginov <victimsnino@gmail.com>
2022-12-05 09:21:55 +0300

    Update publish_subject.hpp

--- src/rpp/rpp/subjects/publish_subject.hpp
+++ src/rpp/rpp/subjects/publish_subject.hpp
@@ -78,9 +78,9 @@ template<rpp::constraint::decayed_type T>
 class publish_subject final : public details::base_subject<T, details::publish_strategy<T>>{
 public:
     publish_subject(const composite_subscription& sub)
-        : details::base_subject<T, details::behavior_strategy<T>>{sub} {}
+        : details::base_subject<T, details::publish_strategy<T>>{sub} {}
 
     publish_subject(composite_subscription&& sub = composite_subscription{})
-        : details::base_subject<T, details::behavior_strategy<T>>{std::move(sub)} {}
+        : details::base_subject<T, details::publish_strategy<T>>{std::move(sub)} {}
 };
 } // namespace rpp::subjects
