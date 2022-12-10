e4b23f6caa3290cd7a7a4bcc689424082d9b9b08
Aleksey Loginov <victimsnino@gmail.com>
2022-12-10 23:36:59 +0300

    Initial implementation for debounce

---
+++ src/rpp/rpp/operators/debounce.hpp
@@ -0,0 +1,147 @@
+//                  ReactivePlusPlus library
+//
+//          Copyright Aleksey Loginov 2022 - present.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/victimsnino/ReactivePlusPlus
+//
+
+#pragma once
+
+#include <rpp/operators/lift.hpp>          // required due to operator uses lift
+#include <rpp/operators/details/early_unsubscribe.hpp>
+#include <rpp/operators/details/serialized_subscriber.hpp>
+#include <rpp/operators/details/subscriber_with_state.hpp>
+#include <rpp/operators/fwd/debounce.hpp>
+#include <rpp/subscribers/constraints.hpp>
+
+#include <rpp/utils/spinlock.hpp>
+
+#include <atomic>
+#include <variant>
+
+IMPLEMENTATION_FILE(debounce_tag);
+
+namespace rpp::details
+{
+template<typename T, typename Scheduler>
+class debounce_state : early_unsubscribe_state
+{
+public:
+    debounce_state(schedulers::duration period, const Scheduler& scheduler, const composite_subscription& subscription_of_subscriber)
+        : early_unsubscribe_state(subscription_of_subscriber)
+        , m_period{period}
+        , m_worker{scheduler.create_worker(children_subscriptions)} {}
+
+    std::optional<schedulers::time_point> emplace_safe(auto&& v)
+    {
+        std::lock_guard lock{m_mutex};
+        m_value_to_be_emitted.emplace(std::forward<decltype(v)>(v));
+        const bool need_to_scheduled = !m_time_when_value_should_be_emitted.has_value() || !m_value_to_be_emitted.has_value();
+        m_time_when_value_should_be_emitted = m_worker.now() + m_period;
+        return need_to_scheduled ? m_time_when_value_should_be_emitted : std::optional<schedulers::time_point>{};
+    }
+
+    std::variant<std::monostate, T, schedulers::duration> extract_value_or_time(schedulers::time_point expected_time)
+    {
+        std::lock_guard lock{m_mutex};
+        if (!m_time_when_value_should_be_emitted.has_value() || !m_value_to_be_emitted.has_value())
+            return std::monostate{};
+
+        if (m_time_when_value_should_be_emitted != expected_time)
+            return m_time_when_value_should_be_emitted.value() - Worker::now();
+
+        m_time_when_value_should_be_emitted.reset();
+        auto v = std::move(m_value_to_be_emitted).value();
+        m_value_to_be_emitted.reset();
+        return v;
+    }
+
+    std::optional<T> extract_value()
+    {
+        std::lock_guard lock{m_mutex};
+        std::optional<T> res{};
+        m_value_to_be_emitted.swap(res);
+        return res;
+    }
+
+private:
+    using Worker = std::decay_t<std::invoke_result_t<decltype(&Scheduler::create_worker), composite_subscription>>;
+
+    schedulers::duration                  m_period;
+    Worker                                m_worker;
+    std::mutex                            m_mutex{};
+    std::optional<schedulers::time_point> m_time_when_value_should_be_emitted{};
+    std::optional<T>                      m_value_to_be_emitted{};
+};
+
+struct debounce_on_next
+{
+    template<typename Value>
+    void operator()(Value&& v, const auto& subscriber, const auto& state_ptr) const
+    {
+        if (const auto time_to_schedule = state_ptr->emplace_safe(std::forward<Value>(v)))
+        {
+            state_ptr->worker.schedule(time_to_schedule.value(),
+                                       [expected_time = time_to_schedule.value(), =]() mutable -> rpp::schedulers::optional_duration
+                                       {
+                                           auto value_or_duration = state_ptr->extract_value_or_time(expected_time);
+                                           if (auto* duration = std::get_if<schedulers::time_point>(&value_or_duration))
+                                               return *duration;
+
+                                           if (auto* value = std::get_if<std::decay_t<Value>>(&value_or_duration))
+                                               subscriber.on_next(std::move(*value));
+                                           return {};
+                                       });
+        }
+    }
+};
+
+using debounce_on_error = early_unsubscribe_on_error;
+
+struct debounce_on_completed
+{
+    void operator()(const auto& subscriber, const auto& state_ptr) const
+    {
+        state_ptr->children_subscriptions.unsubscribe();
+
+        if (auto v = state_ptr->extract_value())
+            subscriber.on_next(std::move(v.value()));
+
+        subscriber.on_completed();
+    }
+};
+
+template<typename T, typename Scheduler>
+struct debounce_state_with_serialized_spinlock : debounce_state<T, Scheduler>
+{
+    using debounce_state<T, Scheduler>::debounce_state;
+
+    // spinlock because most part of time there is only one thread would be active
+    utils::spinlock spinlock{};
+};
+
+template<constraint::decayed_type Type,schedulers::constraint::scheduler TScheduler>
+struct debounce_impl
+{
+    schedulers::duration period;
+    TScheduler           scheduler;
+
+    template<constraint::subscriber_of_type<Type> TSub>
+    auto operator()(TSub&& in_subscriber) const
+    {
+        auto state = std::make_shared<debounce_state_with_serialized_spinlock<Type, TScheduler>>(period, scheduler, in_subscriber.get_subscription());
+        // change subscriber to serialized to avoid manual using of mutex
+        auto subscriber = make_serialized_subscriber(std::forward<TSub>(in_subscriber), std::shared_ptr<utils::spinlock>{state, &state->spinlock});
+
+        return create_subscriber_with_state<Type>(state->children_subscriptions,
+                                                  debounce_on_next{},
+                                                  debounce_on_error{},
+                                                  debounce_on_completed{},
+                                                  std::move(subscriber),
+                                                  std::move(state));
+    }
+};
+} // namespace rpp::details
---
+++ src/tests/rpp/test_debounce.cpp
@@ -0,0 +1,89 @@
+//                   ReactivePlusPlus library
+// 
+//           Copyright Aleksey Loginov 2022 - present.
+//  Distributed under the Boost Software License, Version 1.0.
+//     (See accompanying file LICENSE_1_0.txt or copy at
+//           https://www.boost.org/LICENSE_1_0.txt)
+// 
+//  Project home: https://github.com/victimsnino/ReactivePlusPlus
+
+#include <mock_observer.hpp>
+#include <test_scheduler.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <rpp/operators/debounce.hpp>
+#include <rpp/subjects/publish_subject.hpp>
+
+SCENARIO("debounce emit only items where timeout reached", "[operators][debounce]")
+{
+    auto           debounce_delay = std::chrono::seconds{2};
+    test_scheduler scheduler{};
+    auto           start = s_current_time;
+
+    GIVEN("subject of items and subscriber subscribed on it via debounce")
+    {
+        auto mock = mock_observer<int>{};
+        auto subj = rpp::subjects::publish_subject<int>{};
+        subj.get_observable().debounce(debounce_delay, scheduler).subscribe(mock);
+        WHEN("emit value")
+        {
+            subj.get_subscriber().on_next(1);
+            THEN("delay scheduled action to track period")
+            {
+                CHECK(scheduler.get_schedulings() == std::vector{start+debounce_delay});
+                CHECK(scheduler.get_executions().empty());
+                CHECK(mock.get_total_on_next_count() == 0);
+                CHECK(mock.get_on_error_count() == 0);
+                CHECK(mock.get_on_completed_count() == 0);
+            }
+            AND_WHEN("scheduler reached delayed time")
+            {
+                scheduler.time_advance(debounce_delay);
+                THEN("emission reached mock")
+                {
+                    CHECK(scheduler.get_schedulings() == std::vector{start+debounce_delay});
+                    CHECK(scheduler.get_executions() == == std::vector{start+debounce_delay});
+                    CHECK(mock.get_received_values() == std::vector{1});
+                    CHECK(mock.get_on_error_count() == 0);
+                    CHECK(mock.get_on_completed_count() == 0);
+                }
+            }
+            AND_WHEN("emit on completed")
+            {
+                subj.get_subscriber().on_completed();
+                THEN("emission reached mock with on completed without schedulable exection")
+                {
+                    CHECK(scheduler.get_schedulings() == std::vector{start+debounce_delay});
+                    CHECK(scheduler.get_executions().empty());
+                    CHECK(mock.get_received_values() == std::vector{1});
+                    CHECK(mock.get_on_error_count() == 0);
+                    CHECK(mock.get_on_completed_count() == 1);
+                }
+            }
+            AND_WHEN("new value emitted before scheduler reached requested time")
+            {
+                scheduler.time_advance(debounce_delay / 2);
+                subj.get_subscriber().on_next(2);
+                THEN("nothing changed immediately")
+                {
+                    CHECK(scheduler.get_schedulings() == std::vector{start+debounce_delay});
+                    CHECK(scheduler.get_executions().empty());
+                    CHECK(mock.get_total_on_next_count() == 0);
+                    CHECK(mock.get_on_error_count() == 0);
+                    CHECK(mock.get_on_completed_count() == 0);
+                }
+                AND_WHEN("scheduler reached originally requested time")
+                {
+                    scheduler.time_advance(debounce_delay / 2);
+                    THEN("delay re-schedule schedulable to new delay timepoint")
+                    {
+                        CHECK(scheduler.get_schedulings() == std::vector{start+debounce_delay, start+debounce_delay/2+debounce_delay});
+                        CHECK(scheduler.get_executions().empty());
+                        CHECK(mock.get_total_on_next_count() == 0);
+                        CHECK(mock.get_on_error_count() == 0);
+                        CHECK(mock.get_on_completed_count() == 0);
+                    }
+                }
+            }
+        }
+    }
+}
