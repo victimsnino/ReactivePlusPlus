a483dff6aff4af39994df95bf4ca73aa5414c5c5
Aleksey Loginov <victimsnino@gmail.com>
2023-01-06 20:22:23 +0300

    header

--- src/rpp/rpp/operators/fwd/window.hpp
+++ src/rpp/rpp/operators/fwd/window.hpp
@@ -23,11 +23,14 @@ namespace rpp::details
 template<constraint::decayed_type Type, constraint::observable_of_type<Type> TObs>
 auto window_impl(TObs&& obs, size_t window_size);
 
+template<constraint::decayed_type Type, constraint::observable_of_type<Type> TObs>
+auto window_with_time_impl(TObs&& obs, rpp::schedulers::duration period);
+
 template<constraint::decayed_type Type, typename SpecificObservable>
 struct member_overload<Type, SpecificObservable, window_tag>
 {
    /**
-    * \brief Subdivide original observable into sub-observables (windowed observables) and emit sub-observables of items instead of original items
+    * \brief Subdivide original observable into sub-observables (windowed observables) and emit sub-observables of items instead of original items. Each new sub-observable emitted each `count`-nth emission (and previous completed)
     * 
     * \marble window
     {
@@ -76,6 +79,58 @@ struct member_overload<Type, SpecificObservable, window_tag>
     auto window(size_t window_size) && requires is_header_included<window_tag, Args...>
     {
         return window_impl<Type>(std::move(*static_cast<SpecificObservable*>(this)), window_size);
+    }
+
+    /**
+    * \brief Subdivide original observable into sub-observables (windowed observables) and emit sub-observables of items instead of original items. Each new sub-observable emitted every `period` of time (and previous completed)
+    * 
+    * \marble window_with_time
+    {
+        source observable    :  +1-2-3----4-5|
+
+        operator "window_with_time(3)" : 
+                            {   
+                                +1-2|
+                                ...+-3-|
+                                ......+---|
+                                .........+4-5|
+                            }
+    }
+    *
+    * \details Actually it is similar to `buffer_with_time` but it emits observable instead of container.
+    *
+    * \param period is period of time when previous observable would be completed and new one emitted
+    *
+    * \return new specific_observable with the window_with_time operator as most recent operator.
+    * \warning #include <rpp/operators/window.hpp>
+    * 
+    * \par Example
+    * \snippet window.cpp window_with_time
+    *   
+    * \par Implementation details:
+    * - <b>On subscribe</b>
+    *    - Allocates one `shared_ptr` to keep internal state
+    *    - Schedules schedulable to complete current observable and emit new one
+    * - <b>OnNext</b>
+    *    - Emits emission via active window-observable
+    * - <b>OnError</b>
+    *    - Just forwards original on_error
+    * - <b>OnCompleted</b>
+    *    - Just forwards original on_completed 
+    *
+    * \ingroup transforming_operators
+    * \see https://reactivex.io/documentation/operators/window.html
+    */
+    template<typename ...Args>
+    auto window_with_time(schedulers::duration period) const & requires is_header_included<window_tag, Args...>
+    {
+        return window_with_time_impl<Type>(*static_cast<const SpecificObservable*>(this), period);
+    }
+
+    template<typename ...Args>
+    auto window_with_time(schedulers::duration period) && requires is_header_included<window_tag, Args...>
+    {
+        return window_with_time_impl<Type>(std::move(*static_cast<SpecificObservable*>(this)), period);
     }
 };
 } // namespace rpp::details
