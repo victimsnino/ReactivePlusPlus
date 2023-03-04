32e6ca0f16bcfcebc6e6c04da94a7cbf4827642f
Aleksey Loginov <victimsnino@gmail.com>
2023-03-04 21:38:51 +0300

    interface

--- src/rpp/rpp/observables/interface_observable.hpp
+++ src/rpp/rpp/observables/interface_observable.hpp
@@ -10,8 +10,8 @@
 #pragma once
 
 #include <rpp/observables/fwd.hpp>
-#include <rpp/observers/fwd.hpp>
-#include <rpp/disposables/fwd.hpp>
+#include <rpp/observers/anonymous_observer.hpp>
+#include <rpp/disposables/composite_disposable.hpp>
 
 namespace rpp
 {
@@ -20,6 +20,34 @@ struct interface_observable
 {
     virtual ~interface_observable() = default;
 
-    virtual composite_disposable subscribe(const interface_observer<Type>& observer) = 0;
+    composite_disposable subscribe(const interface_observer<Type>& observer) const
+    {
+        return subscribe_impl(observer);
+    }
+
+    template<constraint::on_next_fn<Type> TOnNext      = utils::empty_function_t<Type>,
+             constraint::on_error_fn      TOnError     = utils::rethrow_error_t,
+             constraint::on_completed_fn  TOnCompleted = utils::empty_function_t<>>
+    composite_disposable subscribe(TOnNext&& on_next = {}, TOnError&& on_error = {}, TOnCompleted&& on_completed = {}) const
+    {
+        return subscribe(anonymous_observer<Type,
+                                            std::decay_t<TOnNext>,
+                                            std::decay_t<TOnError>,
+                                            std::decay_t<TOnCompleted>>{
+                             std::forward<TOnNext>(on_next),
+                             std::forward<TOnError>(on_error),
+                             std::forward<TOnCompleted>(on_completed)});
+    }
+
+    
+    template<constraint::on_next_fn<Type> TOnNext      = utils::empty_function_t<Type>,
+             constraint::on_completed_fn  TOnCompleted = utils::empty_function_t<>>
+    composite_disposable subscribe(TOnNext&& on_next, TOnCompleted&& on_completed) const
+    {
+        return subscribe(std::forward<TOnNext>(on_next), utils::rethrow_error_t{}, std::forward<TOnCompleted>(on_completed));
+    }
+
+protected:
+    virtual composite_disposable subscribe_impl(const interface_observer<Type>& observer) const = 0;
 };
 } // namespace rpp
