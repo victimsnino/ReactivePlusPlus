dd1bef78eee210a878e594c3c4cdf6cfe2653bcb
Aleksey Loginov <victimsnino@gmail.com>
2023-03-28 17:40:19 +0300

    another coverage mechanism

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -1,41 +1,60 @@
 # ---- Variables ----
 
-if(NOT DEFINED RPP_GCOV_TOOL)
-    set(RPP_GCOV_TOOL "")
-endif()
+if (RPP_COVERAGE_LCOV)
+    if(NOT DEFINED RPP_GCOV_TOOL)
+        set(RPP_GCOV_TOOL "")
+    endif()
 
-# We use variables separate from what CTest uses, because those have
-# customization issues
-set(
-    COVERAGE_TRACE_COMMAND
-    lcov -c -q
-    -o "${PROJECT_BINARY_DIR}/coverage.info"
-    -d "${PROJECT_BINARY_DIR}"
-    --include "${PROJECT_SOURCE_DIR}/*"
-    --exclude "*/tests/*"
-    --gcov-tool "${RPP_GCOV_TOOL}"
-    --rc lcov_branch_coverage=1
-    CACHE STRING
-    "; separated command to generate a trace for the 'coverage' target"
-)
+    # We use variables separate from what CTest uses, because those have
+    # customization issues
+    set(
+        COVERAGE_TRACE_COMMAND
+        lcov -c -q
+        -o "${PROJECT_BINARY_DIR}/coverage.info"
+        -d "${PROJECT_BINARY_DIR}"
+        --include "${PROJECT_SOURCE_DIR}/*"
+        --exclude "*/tests/*"
+        --gcov-tool "${RPP_GCOV_TOOL}"
+        --rc lcov_branch_coverage=1
+        CACHE STRING
+        "; separated command to generate a trace for the 'coverage' target"
+    )
 
-set(
-    COVERAGE_HTML_COMMAND
-    genhtml --legend -f -q
-    "${PROJECT_BINARY_DIR}/coverage.info"
-    -p "${PROJECT_SOURCE_DIR}"
-    -o "${PROJECT_BINARY_DIR}/coverage_html"
-    --rc lcov_branch_coverage=1
-    CACHE STRING
-    "; separated command to generate an HTML report for the 'coverage' target"
-)
+    set(
+        COVERAGE_HTML_COMMAND
+        genhtml --legend -f -q
+        "${PROJECT_BINARY_DIR}/coverage.info"
+        -p "${PROJECT_SOURCE_DIR}"
+        -o "${PROJECT_BINARY_DIR}/coverage_html"
+        --rc lcov_branch_coverage=1
+        CACHE STRING
+        "; separated command to generate an HTML report for the 'coverage' target"
+    )
 
-# ---- Coverage target ----
+    # ---- Coverage target ----
 
+    add_custom_target(
+        coverage
+        COMMAND ${COVERAGE_TRACE_COMMAND}
+        COMMAND ${COVERAGE_HTML_COMMAND}
+        COMMENT "Generating coverage report"
+        VERBATIM
+    )
+else()
 add_custom_target(
     coverage
-    COMMAND ${COVERAGE_TRACE_COMMAND}
-    COMMAND ${COVERAGE_HTML_COMMAND}
+    COMMAND
+        gcovr
+        -r "${PROJECT_SOURCE_DIR}"
+        --object-dir="${PROJECT_BINARY_DIR}"
+        -f src
+        -e src/tests
+        -b --decisions --calls
+        --exclude-unreachable-branches
+        --exclude-throw-branches
+        --html-details "${PROJECT_BINARY_DIR}/coverage_report.html"
+        --gcov-executable "${RPP_GCOV_TOOL}"
     COMMENT "Generating coverage report"
     VERBATIM
 )
+endif()
