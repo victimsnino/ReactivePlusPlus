6e7301c6d54c3df51c9ce5de5340edf9ecad8299
Aleksey Loginov <victimsnino@gmail.com>
2023-03-28 11:58:05 +0300

    fix

--- src/rpp/rpp/observers/dynamic_observer.hpp
+++ src/rpp/rpp/observers/dynamic_observer.hpp
@@ -1,10 +1,10 @@
 //                   ReactivePlusPlus library
-// 
+//
 //           Copyright Aleksey Loginov 2023 - present.
 //  Distributed under the Boost Software License, Version 1.0.
 //     (See accompanying file LICENSE_1_0.txt or copy at
 //           https://www.boost.org/LICENSE_1_0.txt)
-// 
+//
 //  Project home: https://github.com/victimsnino/ReactivePlusPlus
 
 #pragma once
@@ -31,7 +31,7 @@ template<typename Strategy>
 void forwarding_set_upstream(const void* ptr, const composite_disposable& d) { static_cast<const Strategy*>(ptr)->set_upstream(d); };
 
 template<typename Strategy>
-bool forwarding_is_disposed(const void* ptr) { return static_cast<const Strategy*>(ptr)->is_disposed(); };
+bool forwarding_is_disposed(const void* ptr) { return static_cast<const Strategy*>(ptr)->is_disposed(); }
 
 template<constraint::decayed_type Type>
 class dynamic_strategy final
