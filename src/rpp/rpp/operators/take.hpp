5283a75f8ba52c0cd9717449ad5e01de70492fb6
Aleksey Loginov <victimsnino@gmail.com>
2023-04-05 10:31:10 +0300

    Move comment

--- src/rpp/rpp/operators/take.hpp
+++ src/rpp/rpp/operators/take.hpp
@@ -48,29 +48,29 @@ namespace rpp::operators
 template<constraint::observable TObservable>
 using take_observable = details::operator_observable<std::decay_t<TObservable>, details::take_observer_strategy, size_t>;
 
-/**
-* @brief Emit only first `count` items provided by observable, then send `on_completed`
-*
-* @marble take
-{
-    source observable  : +--1-2-3-4-5-6-|
-    operator "take(3)" : +--1-2-3|
-}
-* @details Actually this operator just emits emissions while counter is not zero and decrements counter on each emission
-*
-* @param count amount of items to be emitted. 0 - instant complete
-* @return new specific_observable with the Take operator as most recent operator.
-* @warning #include <rpp/operators/take.hpp>
-*
-* @par Example:
-* @snippet take.cpp take
-*
-* @ingroup filtering_operators
-* @see https://reactivex.io/documentation/operators/take.html
-*/
 class take
 {
 public:
+    /**
+    * @brief Emit only first `count` items provided by observable, then send `on_completed`
+    *
+    * @marble take
+    {
+        source observable  : +--1-2-3-4-5-6-|
+        operator "take(3)" : +--1-2-3|
+    }
+    * @details Actually this operator just emits emissions while counter is not zero and decrements counter on each emission
+    *
+    * @param count amount of items to be emitted. 0 - instant complete
+    * @return new specific_observable with the Take operator as most recent operator.
+    * @warning #include <rpp/operators/take.hpp>
+    *
+    * @par Example:
+    * @snippet take.cpp take
+    *
+    * @ingroup filtering_operators
+    * @see https://reactivex.io/documentation/operators/take.html
+    */
     take(size_t count)
         : m_count{count} {}
 
