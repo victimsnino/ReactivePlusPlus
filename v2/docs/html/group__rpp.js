8697bf112ab556989002c4be0f7874fc32c36882
victimsnino <victimsnino@users.noreply.github.com>
2023-04-06 15:42:32 +0000

    deploy: b65fde0175b6c42d6e69d4f6a74abfa1fdadb7ab

--- v2/docs/html/group__rpp.js
+++ v2/docs/html/group__rpp.js
@@ -1,6 +1,6 @@
 var group__rpp =
 [
-    [ "Observables", "group__observables.html", null ],
+    [ "Observables", "group__observables.html", "group__observables" ],
     [ "Observers", "group__observers.html", null ],
     [ "Operators", "group__operators.html", "group__operators" ]
 ];
