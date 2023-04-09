981f47172c56d66cb5f5bf14837cab782df2d003
Aleksey Loginov <victimsnino@gmail.com>
2023-04-09 12:34:41 +0300

    examples (#330)

--- src/rpp/rpp/operators/subscribe.hpp
+++ src/rpp/rpp/operators/subscribe.hpp
@@ -14,6 +14,7 @@
 #include <rpp/observers/base_observer.hpp>
 #include <rpp/observables/fwd.hpp>
 #include <rpp/disposables/composite_disposable.hpp>
+#include <rpp/utils/functors.hpp>
 #include <utility>
 
 namespace rpp::operators
@@ -102,8 +103,9 @@ public:
     /**
      * @brief Construct rpp::lambda_observer on the fly and subscribe it to emissions from this observable
      */
-    template<rpp::constraint::decayed_same_as<OnNext> TOnNext, rpp::constraint::decayed_same_as<OnError> TOnError, rpp::constraint::decayed_same_as<OnCompleted> TOnCompleted>
-    explicit subscribe(TOnNext&& on_next, TOnError&& on_error, TOnCompleted&& on_completed)
+    template<rpp::constraint::decayed_same_as<OnNext> TOnNext, rpp::constraint::decayed_same_as<OnError> TOnError = OnError, rpp::constraint::decayed_same_as<OnCompleted> TOnCompleted = OnCompleted>
+        requires (!constraint::decayed_same_as<TOnNext, subscribe<OnNext, OnError, OnCompleted>>)
+    explicit subscribe(TOnNext&& on_next, TOnError&& on_error = {}, TOnCompleted&& on_completed = {}) // NOLINT
         : m_on_next{std::forward<TOnNext>(on_next)}
         , m_on_error{std::forward<TOnError>(on_error)}
         , m_on_completed{std::forward<TOnCompleted>(on_completed)}
@@ -131,6 +133,20 @@ private:
     RPP_NO_UNIQUE_ADDRESS OnCompleted m_on_completed;
 };
 
+template<typename OnNext>
+class subscribe<OnNext> : public subscribe<OnNext, rpp::utils::rethrow_error_t, rpp::utils::empty_function_t<>>
+{
+public:
+    using subscribe<OnNext, rpp::utils::rethrow_error_t, rpp::utils::empty_function_t<>>::subscribe;
+};
+
+template<typename OnNext, std::invocable<const std::exception_ptr&> OnError>
+class subscribe<OnNext, OnError> : public subscribe<OnNext, OnError, rpp::utils::empty_function_t<>>
+{
+public:
+    using subscribe<OnNext, OnError, rpp::utils::empty_function_t<>>::subscribe;
+};
+
 template<typename OnNext, std::invocable<const std::exception_ptr&> OnError, std::invocable<> OnCompleted>
 class subscribe<rpp::composite_disposable, OnNext, OnError, OnCompleted>
 {
@@ -138,8 +154,8 @@ public:
     /**
      * @brief Construct rpp::lambda_observer on the fly and subscribe it to emissions from this observable
      */
-    template<rpp::constraint::decayed_same_as<OnNext> TOnNext, rpp::constraint::decayed_same_as<OnError> TOnError, rpp::constraint::decayed_same_as<OnCompleted> TOnCompleted>
-    explicit subscribe(rpp::composite_disposable d, TOnNext&& on_next, TOnError&& on_error, TOnCompleted&& on_completed)
+    template<rpp::constraint::decayed_same_as<OnNext> TOnNext, rpp::constraint::decayed_same_as<OnError> TOnError = OnError, rpp::constraint::decayed_same_as<OnCompleted> TOnCompleted = OnCompleted>
+    explicit subscribe(rpp::composite_disposable d, TOnNext&& on_next, TOnError&& on_error = {}, TOnCompleted&& on_completed = {})
         : m_disposable{std::move(d)}
         , m_on_next{std::forward<TOnNext>(on_next)}
         , m_on_error{std::forward<TOnError>(on_error)}
@@ -171,6 +187,20 @@ private:
     RPP_NO_UNIQUE_ADDRESS OnNext                    m_on_next;
     RPP_NO_UNIQUE_ADDRESS OnError                   m_on_error;
     RPP_NO_UNIQUE_ADDRESS OnCompleted               m_on_completed;
+};
+
+template<typename OnNext>
+class subscribe<rpp::composite_disposable, OnNext> : public subscribe<rpp::composite_disposable, OnNext, rpp::utils::rethrow_error_t, rpp::utils::empty_function_t<>>
+{
+public:
+    using subscribe<rpp::composite_disposable, OnNext, rpp::utils::rethrow_error_t, rpp::utils::empty_function_t<>>::subscribe;
+};
+
+template<typename OnNext, std::invocable<const std::exception_ptr&> OnError>
+class subscribe<rpp::composite_disposable, OnNext, OnError> : public subscribe<rpp::composite_disposable, OnNext, OnError, rpp::utils::empty_function_t<>>
+{
+public:
+    using subscribe<rpp::composite_disposable, OnNext, OnError, rpp::utils::empty_function_t<>>::subscribe;
 };
 
 template<typename ...Args>
