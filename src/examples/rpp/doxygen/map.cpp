6c69633bb3d86b1092c4c9f0c8b7545a46f693f4
Aleksey Loginov <victimsnino@gmail.com>
2023-04-11 00:23:19 +0300

    fix

--- src/examples/rpp/doxygen/map.cpp
+++ src/examples/rpp/doxygen/map.cpp
@@ -9,21 +9,15 @@
 int main() // NOLINT
 {
     //! [Same type]
-    rpp::source::just(42)
-            | rpp::operators::map{[](int value)
-            {
-                return value + 10;
-            }}
-            | rpp::operators::subscribe([](int v) { std::cout << v << std::endl; });
+    rpp::source::just(42) 
+        | rpp::operators::map([](int value) { return value + 10; }) 
+        | rpp::operators::subscribe([](int v) { std::cout << v << std::endl; });
     // Output: 52
     //! [Same type]
 
     //! [Changed type]
     rpp::source::just(42)
-            | rpp::operators::map([](int value)
-            {
-                return std::to_string(value) + " VAL";
-            })
+            | rpp::operators::map([](int value) { return std::to_string(value) + " VAL"; })
             | rpp::operators::subscribe([](const std::string& v) { std::cout << v << std::endl; });
     // Output: 42 VAL
     //! [Changed type]
