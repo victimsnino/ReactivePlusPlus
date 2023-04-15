3d6c040b8eb08e9246271d4d37466b7b2651ef79
Aleksey Loginov <victimsnino@gmail.com>
2023-04-15 13:53:11 +0300

    fix

--- src/rpp/rpp/operators/details/strategy.hpp
+++ src/rpp/rpp/operators/details/strategy.hpp
@@ -64,7 +64,7 @@ public:
     operator_strategy_base(const operator_strategy_base&) = delete;
     operator_strategy_base(operator_strategy_base&& other) noexcept
         : m_observer{std::in_place_index<1>, std::move(other).get_observer()}
-        , m_strategy{std::move(other.strategy)}{}
+        , m_strategy{std::move(other.m_strategy)}{}
 
     operator_strategy_base& operator=(const operator_strategy_base&) = delete;
     operator_strategy_base& operator=(operator_strategy_base&&) = delete;
