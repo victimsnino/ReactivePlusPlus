b726b085784695148ced4b630485275af1c268ef
Aleksey Loginov <victimsnino@gmail.com>
2023-04-15 14:02:30 +0300

    save

--- src/rpp/rpp/operators/details/strategy.hpp
+++ src/rpp/rpp/operators/details/strategy.hpp
@@ -58,46 +58,28 @@ public:
      */
     template<typename...Args>
     operator_strategy_base(observer&& observer, Args&&...args)
-        : m_observer{std::in_place_index<0>, observer}
+        : m_observer{std::move(observer)}
         , m_strategy{std::forward<Args>(args)...} {}
 
     operator_strategy_base(const operator_strategy_base&) = delete;
-    operator_strategy_base(operator_strategy_base&& other) noexcept
-        : m_observer{std::in_place_index<1>, std::move(other).get_observer()}
-        , m_strategy{std::move(other.m_strategy)}{}
+    operator_strategy_base(operator_strategy_base&& other) noexcept = default;
 
     operator_strategy_base& operator=(const operator_strategy_base&) = delete;
     operator_strategy_base& operator=(operator_strategy_base&&) = delete;
 
     ~operator_strategy_base() noexcept = default;
 
-    void set_upstream(const disposable_wrapper& d)     { m_strategy.set_upstream(get_observer(), d); }
-    bool is_disposed() const                           { return m_strategy.is_disposed() || get_observer().is_disposed(); }
+    void set_upstream(const disposable_wrapper& d)     { m_strategy.set_upstream(m_observer, d); }
+    bool is_disposed() const                           { return m_strategy.is_disposed() || m_observer.is_disposed(); }
 
-    void on_next(const T& v) const                     { m_strategy.on_next(get_observer(), v); }
-    void on_next(T&& v) const                          { m_strategy.on_next(get_observer(), std::move(v)); }
-    void on_error(const std::exception_ptr& err) const { m_strategy.on_error(get_observer(), err); }
-    void on_completed() const                          { m_strategy.on_completed(get_observer()); }
-
-private:
-    observer&& get_observer() &&
-    {
-        return std::visit([](observer& v) -> observer&& {return std::move(v);}, m_observer);
-    }
-
-    observer& get_observer() &
-    {
-        return std::visit([](observer& v) -> observer& {return v;}, m_observer);
-    }
-
-    const observer& get_observer() const &
-    {
-        return std::visit([](const observer& v) -> const observer& {return v;}, m_observer);
-    }
+    void on_next(const T& v) const                     { m_strategy.on_next(m_observer, v); }
+    void on_next(T&& v) const                          { m_strategy.on_next(m_observer, std::move(v)); }
+    void on_error(const std::exception_ptr& err) const { m_strategy.on_error(m_observer, err); }
+    void on_completed() const                          { m_strategy.on_completed(m_observer); }
 
 private:
-    std::variant<std::reference_wrapper<observer>, observer> m_observer;
-    RPP_NO_UNIQUE_ADDRESS Strategy                           m_strategy;
+    observer                       m_observer;
+    RPP_NO_UNIQUE_ADDRESS Strategy m_strategy;
 };
 
 struct forwarding_on_next_strategy
