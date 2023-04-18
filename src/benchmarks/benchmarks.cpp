58d4c63e50f5b34e5c6b7a8ecab00008fd051eed
Aleksey Loginov <victimsnino@gmail.com>
2023-04-18 11:42:38 +0300

    Update benchmarks.cpp

--- src/benchmarks/benchmarks.cpp
+++ src/benchmarks/benchmarks.cpp
@@ -137,7 +137,7 @@ int main(int argc, char* argv[]) // NOLINT
 
     BENCHMARK("Filtering Operators")
     {
-        SECTION("take(1)")
+        SECTION("create+take(1)+subscribe")
         {
             TEST_RPP([&]()
             {
