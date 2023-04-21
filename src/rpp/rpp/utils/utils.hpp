6bb48dc0e7ab2f224f5ed7b3ce3cb7f6440a3199
Aleksey Loginov <victimsnino@gmail.com>
2023-04-21 15:55:44 +0300

    Update utils.hpp

--- src/rpp/rpp/utils/utils.hpp
+++ src/rpp/rpp/utils/utils.hpp
@@ -15,32 +15,11 @@
 
 #include <concepts>
 #include <iterator>
-#include <mutex>
-#include <thread>
 
 namespace rpp::utils {
 
 template<constraint::iterable T>
 using iterable_value_t = std::iter_value_t<decltype(std::begin(std::declval<T>()))>;
-
-struct none_lock
-{
-    static void lock() {}
-    static void unlock() {}
-};
-
-struct none_condition_variable
-{
-    static void notify_one() {}
-    static void wait(std::unique_lock<none_lock>&, const auto&) {}
-
-    template<typename _Clock, typename _Duration>
-    static bool wait_until(std::unique_lock<none_lock>&, const std::chrono::time_point<_Clock, _Duration>& time, const auto& predicate)
-    {
-        std::this_thread::sleep_until(time);
-        return predicate();
-    }
-};
 
 /**
  * @brief Calls passed function during destruction
