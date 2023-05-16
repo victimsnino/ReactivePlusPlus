af1a5e19f657c408bb520bc6a5e9c1966e4870da
Aleksey Loginov <victimsnino@gmail.com>
2023-05-16 10:34:40 +0300

    Update test_observers.cpp

--- src/tests/rpp/test_observers.cpp
+++ src/tests/rpp/test_observers.cpp
@@ -182,7 +182,7 @@ TEST_CASE("set_upstream can be called multiple times")
     {
         auto d1 = std::make_shared<rpp::base_disposable>();
         observer.set_upstream(rpp::disposable_wrapper{d1});
-        CHECK(!d1->is_disposed());
+        CHECK(d1->is_disposed() == observer.is_disposed());
         observer.set_upstream(rpp::disposable_wrapper{std::make_shared<rpp::base_disposable>()});
         CHECK(d1->is_disposed());
     };
