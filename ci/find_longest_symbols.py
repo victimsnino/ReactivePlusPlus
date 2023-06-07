a1359dafac99a3319ca926994dd13e70c269c350
Aleksey Loginov <victimsnino@gmail.com>
2023-06-07 14:14:32 +0300

    Cleanup binary a bit (#372)

---
+++ ci/find_longest_symbols.py
@@ -0,0 +1,31 @@
+
+def indent(l):
+    res = ""
+    indent = 0
+    for c in l:
+        if c == "<":
+            indent += 1
+            res += c + "\n"+"\t"*indent
+        elif c == ">":
+            indent -= 1
+            res += "\n"+"\t"*indent + c
+        elif c == ",":
+            res += "\n"+"\t"*indent + c
+        else:
+            res += c
+    return res
+
+with open("../build/bin/dump.txt") as f:
+    data = f.readlines()
+
+data = [l for l in data if "rpp" in l]
+data = [l for l in data if "simulate_" not in l]
+
+print(f"Total lines: {len(data)}")
+
+data = sorted(data, key=len, reverse=True)
+
+print(f"Longest line: {len(data[0])}")
+
+res = indent(data[0])
+print(res)
