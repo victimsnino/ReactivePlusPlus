193db0625598a644d2d5715a042473e4e86f949f
Aleksey Loginov <victimsnino@gmail.com>
2023-08-18 23:54:11 +0300

    one more way

--- src/benchmarks/benchmarks.cpp
+++ src/benchmarks/benchmarks.cpp
@@ -10,14 +10,8 @@
 #include <rpp/rpp.hpp>
 #ifdef RPP_BUILD_RXCPP
     #include <rxcpp/rx.hpp>
-    namespace rxcpp::util
-    {
-        template<class... T>
-        inline auto surely(const std::tuple<T...>& tpl)
-            -> decltype(rxcpp::util::apply(tpl, rxcpp::util::detail::surely())) {
-            return      rxcpp::util::apply(tpl, rxcpp::util::detail::surely());
-        }
-    }
+    using namespace std;
+    using namespace rxcpp::util;
 #endif
 
 #define BENCHMARK(NAME)     bench.context("benchmark_title", NAME);
