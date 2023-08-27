32763c6655656f6063f9cd5a87fc1e5870afe546
Corentin <corentin.bugnot@etu.unige.ch>
2023-08-27 12:55:06 +0200

    Better doxygen example

--- src/examples/rpp/doxygen/defer.cpp
+++ src/examples/rpp/doxygen/defer.cpp
@@ -9,7 +9,33 @@
 int main() // NOLINT
 {
     //! [defer from_iterable]
-    rpp::source::defer([] { return rpp::source::from_iterable(std::vector<int>{ 1,2,3 }); }).subscribe([](int v) {std::cout << v << " "; });
-    // Output: 1 2 3
+    rpp::source::defer([] {
+        std::cout << "Observable factory called\n";
+        return rpp::source::from_iterable(std::vector<int>{ 1,2,3 }); })
+        .subscribe([](int v) { std::cout << v << "\n"; }, rpp::utils::rethrow_error_t{}, []() { std::cout << "On complete\n"; });
+    // Output: Observable factory called 
+    //         1 
+    //         2 
+    //         3
+    //         On complete
     //! [defer from_iterable]
+    
+    //! [defer mutable source]
+    auto obs = rpp::source::defer([] {
+        std::cout << "Observable factory called\n";
+        auto inner_obs = rpp::source::create<int>([state = std::make_shared<int>(0)](const auto& obs) {
+            obs.on_next((*state)++);
+            obs.on_completed();
+        });  
+        return inner_obs; 
+    });
+    obs.subscribe([](int v) { std::cout << v << "\n"; }, rpp::utils::rethrow_error_t{}, []() { std::cout << "On complete\n"; });
+    obs.subscribe([](int v) { std::cout << v << "\n"; }, rpp::utils::rethrow_error_t{}, []() { std::cout << "On complete\n"; });
+    // Output: Observable factory called 
+    //         0
+    //         On complete
+    // Output: Observable factory called 
+    //         0
+    //         On complete
+    //! [defer mutable source]
 }
