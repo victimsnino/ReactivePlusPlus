3de3ba7e2623149061ac0fd24c822277b20ea071
Corentin <corentin.bugnot@etu.unige.ch>
2023-08-31 22:44:22 +0200

    Fix

--- src/examples/rpp/doxygen/interval.cpp
+++ src/examples/rpp/doxygen/interval.cpp
@@ -9,18 +9,17 @@
 
 int main() // NOLINT
 {
-    auto start = rpp::schedulers::clock_type::now();
     //! [interval period]
     rpp::source::interval(std::chrono::milliseconds(10), rpp::schedulers::immediate{})
         | rpp::operators::take(3)
         | rpp::operators::subscribe(
-            [&](size_t v) { std::cout << "emit " << v << " duration since start " << std::chrono::duration_cast<std::chrono::milliseconds>(rpp::schedulers::clock_type::now() - start).count() << "ms\n"; }, 
+            [start = rpp::schedulers::clock_type::now()](size_t v) { std::cout << "emit " << v << " duration since start " << std::chrono::duration_cast<std::chrono::milliseconds>(rpp::schedulers::clock_type::now() - start).count() << "ms\n"; }, 
             rpp::utils::rethrow_error_t{}, 
             []() { std::cout << "On complete\n"; });
     // Output: Observable factory called 
-    //    emit 1 duration since start 1ms
-    //    emit 2 duration since start 15ms
-    //    emit 3 duration since start 30ms
+    //    emit 1 duration since start 0ms
+    //    emit 2 duration since start 10ms
+    //    emit 3 duration since start 20ms
     //    On complete
     //! [defer from_iterable]
     
@@ -32,9 +31,9 @@ int main() // NOLINT
             rpp::utils::rethrow_error_t{}, 
             []() { std::cout << "On complete\n"; });
     // Output: Observable factory called 
-    //    emit 1 duration since start 15ms
-    //    emit 2 duration since start 30ms
-    //    emit 3 duration since start 45ms
+    //    emit 1 duration since start 5ms
+    //    emit 2 duration since start 15ms
+    //    emit 3 duration since start 25ms
     //    On complete
     //! [defer from_iterable]
 }
