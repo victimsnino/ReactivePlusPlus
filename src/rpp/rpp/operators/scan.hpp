77a3dce215a0d9625fde48b112207c1eb2ff6952
Aleksey Loginov <victimsnino@gmail.com>
2023-09-14 22:57:14 +0300

    enable clang-tidy

--- src/rpp/rpp/operators/scan.hpp
+++ src/rpp/rpp/operators/scan.hpp
@@ -33,9 +33,9 @@ struct scan_observer_strategy
     RPP_NO_UNIQUE_ADDRESS Fn           fn;
 
     RPP_CALL_DURING_CONSTRUCTION(
-        {
-            observer.on_next(utils::as_const(seed));
-        });
+    {
+        observer.on_next(utils::as_const(seed));
+    });
 
     template<typename T>
     void on_next(T&& v) const
