9202a40c2e885e461e93f2ba666075b9c22229b6
Aleksey Loginov <victimsnino@gmail.com>
2023-11-19 16:14:20 +0300

    updatew

--- src/examples/rpp/two_async_streams/two_async_streams.cpp
+++ src/examples/rpp/two_async_streams/two_async_streams.cpp
@@ -41,9 +41,10 @@ int main() // NOLINT(bugprone-exception-escape)
         | rpp::operators::map([](size_t i) { return format_message(std::string{"counter "} + std::to_string(i)); })
         | rpp::operators::merge_with(chars)
         | rpp::operators::observe_on(rpp::schedulers::new_thread{})
+        | rpp::operators::take_until(termination)
         | rpp::operators::as_blocking()
         | rpp::operators::subscribe([](const std::string& event) {
-              std::cout << format_message(event) << std::endl;
+              std::cout << event << std::endl;
           });
     
     std::cout << "EXIT" << std::endl;
