986aea6f33ac06a25a2cede8f60a797882dff221
Aleksey Loginov <victimsnino@gmail.com>
2023-11-19 15:00:11 +0300

    add new_example

--- src/examples/rpp/two_async_streams/two_async_streams.cpp
+++ src/examples/rpp/two_async_streams/two_async_streams.cpp
@@ -12,20 +12,40 @@ std::string format_message(T data) {
     ss << "[" 
               << std::this_thread::get_id() 
               << "][" << std::chrono::duration_cast<std::chrono::milliseconds>(rpp::schedulers::clock_type::now() - start).count() 
-              << "] Message: " 
-              << data
-              << std::endl;
+              << "ms] Message: " 
+              << data;
     return ss.str();
 }
 
 int main() // NOLINT(bugprone-exception-escape)
 {
-    auto keyboard_events = rpp::source::from_callable(&::getchar)
-                         | rpp::operators::repeat()
-                         | rpp::operators::take_while([](char v) { return v != '0'; })
-                         | rpp::operators::filter(std::not_fn(&::isdigit))
-                         | rpp::operators::map(&::toupper)
-                         | rpp::operators::map(format_message<char>);
+    auto raw_keyboard_events = rpp::source::from_callable(&::getchar)
+                             | rpp::operators::repeat()
+                             | rpp::operators::subscribe_on(rpp::schedulers::new_thread{})
+                             | rpp::operators::publish()
+                             | rpp::operators::ref_count();
+
+    auto termination = raw_keyboard_events
+                     | rpp::operators::filter([](char v) { return v == '0'; });
+
+    auto chars = raw_keyboard_events
+               | rpp::operators::filter([](char v) { return !std::isdigit(v) && v != '\n';})
+               | rpp::operators::map(&::toupper)
+               | rpp::operators::map(format_message<char>);
+
+
+    start = rpp::schedulers::clock_type::now();
+    std::cout << format_message("main thread") << std::endl;
+
+    rpp::source::interval(std::chrono::seconds{1}, rpp::schedulers::new_thread{})
+        | rpp::operators::map([](size_t i) { return format_message(std::string{"counter "} + std::to_string(i)); })
+        | rpp::operators::merge_with(chars)
+        | rpp::operators::as_blocking()
+        | rpp::operators::subscribe([](const std::string& event) {
+              std::cout << event << std::endl;
+          });
+    
+    std::cout << "EXIT" << std::endl;
 
     return 0;
 }
