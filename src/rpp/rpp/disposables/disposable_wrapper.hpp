2f9e1085dabfb26e88021b2f85e85a3f2de34d5b
Aleksey Loginov <victimsnino@gmail.com>
2023-12-04 10:24:12 +0300

    Update disposable_wrapper.hpp

--- src/rpp/rpp/disposables/disposable_wrapper.hpp
+++ src/rpp/rpp/disposables/disposable_wrapper.hpp
@@ -56,9 +56,6 @@ public:
     {
     }
 
-    disposable_wrapper_impl(const disposable_wrapper_impl&) = default;
-    disposable_wrapper_impl(disposable_wrapper_impl&&) noexcept = default;
-
     static disposable_wrapper_impl from_shared(std::shared_ptr<TDisposable> disposable)
     {
         return disposable_wrapper_impl{std::move(disposable)};
