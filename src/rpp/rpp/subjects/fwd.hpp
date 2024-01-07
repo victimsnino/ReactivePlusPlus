8a4e6252bbd995ff018e8784f38411482b97dd24
Aleksey Loginov <victimsnino@gmail.com>
2024-01-07 19:09:12 +0300

    fix

--- src/rpp/rpp/subjects/fwd.hpp
+++ src/rpp/rpp/subjects/fwd.hpp
@@ -49,15 +49,23 @@ namespace rpp::subjects::utils
 {
 namespace details
 {
-    template<typename T>
-    struct extract_subject_type : std::false_type
+    template<typename TSubject>
+    struct extract_subject_type 
     {
+        template<typename TT, typename Strategy>
+        constexpr static TT deduce(const rpp::subjects::details::base_subject<TT, Strategy>&);
+
+        using type = decltype(deduce(std::declval<std::decay_t<TSubject>>()));
     };
 
-    template<typename TT, typename Strategy>
-    struct extract_subject_type<rpp::subjects::details::base_subject<TT, Strategy>> : std::true_type
+    template<typename TSubject>
+    struct is_subject_t
     {
-        using type = TT;
+        template<typename TT, typename Strategy>
+        constexpr static std::true_type  deduce(const rpp::subjects::details::base_subject<TT, Strategy>&);
+        constexpr static std::false_type deduce(...);
+
+        using type = decltype(deduce(std::declval<std::decay_t<TSubject>*>()));
     };
 
 } // namespace details
@@ -69,5 +77,5 @@ using extract_subject_type_t = typename details::extract_subject_type<std::decay
 namespace rpp::constraint
 {
 template<typename T>
-concept subject = rpp::subjects::utils::details::extract_subject_type<std::decay_t<T>>::value;
+concept subject = rpp::subjects::utils::details::is_subject_t<std::decay_t<T>>::type::value;
 }
