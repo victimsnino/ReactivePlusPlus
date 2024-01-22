be0ef72621a07b49a024e4314dab049bd0dd7cc3
Aleksey Loginov <victimsnino@gmail.com>
2024-01-22 17:23:07 +0300

    Make static container without array

--- src/rpp/rpp/disposables/details/container.hpp
+++ src/rpp/rpp/disposables/details/container.hpp
@@ -12,7 +12,6 @@
 #include <rpp/disposables/disposable_wrapper.hpp>
 #include <rpp/utils/exceptions.hpp>
 
-#include <array>
 #include <algorithm>
 #include <vector>
 
@@ -61,7 +60,7 @@ template<size_t Count>
 class dynamic_disposables_container : public dynamic_disposables_container_base
 {
 public:
-    dynamic_disposables_container() 
+    dynamic_disposables_container()
         : dynamic_disposables_container_base{Count}
     {}
 };
@@ -71,26 +70,61 @@ class static_disposables_container
 {
 public:
     static_disposables_container() = default;
+    static_disposables_container(const static_disposables_container&) = delete;
+    static_disposables_container& operator=(const static_disposables_container& other) = delete;
+
+    static_disposables_container& operator=(static_disposables_container&& other) noexcept
+    {
+        if (this == &other)
+            return *this;
+
+        m_size = other.m_size;
+        for(size_t i =0; i < m_size; ++i)
+            std::construct_at(get(i), std::move(*other.get(i)));
+
+        other.clear();
+        return *this;
+    }
+
+    static_disposables_container(static_disposables_container&& other) noexcept
+    {
+        *this = std::move(other);
+    }
+
+    ~static_disposables_container() noexcept
+    {
+        clear();
+    }
 
     void push_back(const rpp::disposable_wrapper& d)
     {
         if (m_size >= Count)
             throw rpp::utils::more_disposables_than_expected{"static_disposables_container obtained more disposables than expected"};
-        m_data[m_size++] = d;
+        std::construct_at(get(m_size++), d);
     }
 
     void push_back(rpp::disposable_wrapper&& d)
     {
         if (m_size >= Count)
             throw rpp::utils::more_disposables_than_expected{"static_disposables_container obtained more disposables than expected"};
-        m_data[m_size++] = std::move(d);
+        std::construct_at(get(m_size++), std::move(d));
     }
 
     void remove(const rpp::disposable_wrapper& d)
     {
-        auto itr = std::remove(m_data.begin(), m_data.end(), d);
-        while(itr != m_data.end()) {
-            (*itr++) = disposable_wrapper{};
+        for (size_t i = 0; i < m_size;)
+        {
+            if (*get(i) != d)
+            {
+                ++i;
+                continue;
+            }
+
+            for(size_t j = i+1; j < m_size; ++j)
+            {
+                std::construct_at(get(j-1), std::move(*get(j)));
+                std::destroy_at(get(j));
+            }
             --m_size;
         }
     }
@@ -98,19 +132,26 @@ public:
     void dispose() const
     {
         for (size_t i =0; i < m_size; ++i) {
-            m_data[i].dispose();
+            get(i)->dispose();
         }
     }
 
     void clear()
     {
-        m_data = std::array<rpp::disposable_wrapper, Count>{};
+        for (size_t i =0; i < m_size; ++i)
+            std::destroy_at(get(i));
         m_size = 0;
     }
 
 private:
-    mutable std::array<rpp::disposable_wrapper, Count> m_data{};
-    size_t                                             m_size{};
+    const rpp::disposable_wrapper* get(size_t i) const
+    {
+        return std::launder(reinterpret_cast<const rpp::disposable_wrapper*>(&m_data[i*sizeof(rpp::disposable_wrapper)]));
+    }
+
+private:
+    alignas(rpp::disposable_wrapper) std::byte m_data[sizeof(rpp::disposable_wrapper) * Count]{};
+    size_t m_size{};
 };
 
 struct none_disposables_container
