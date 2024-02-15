5a3b1d3956fa384d5fee8bca6b3dfb1e726b9d71
Aleksey Loginov <victimsnino@gmail.com>
2024-02-15 16:15:54 +0300

    revert map

--- src/examples/rpp/doxygen/map.cpp
+++ src/examples/rpp/doxygen/map.cpp
@@ -10,15 +10,15 @@ int main() // NOLINT(bugprone-exception-escape)
 {
     //! [Same type]
     rpp::source::just(42)
-        | rpp::operators::map([](std::string value) { return value; });
-        // | rpp::operators::subscribe([](int v) { std::cout << v << std::endl; });
+        | rpp::operators::map([](int value) { return value + 10; })
+        | rpp::operators::subscribe([](int v) { std::cout << v << std::endl; });
     // Output: 52
     //! [Same type]
 
     //! [Changed type]
-    rpp::source::just(std::vector<int>{})
-            | rpp::operators::distinct();
-            // | rpp::operators::subscribe([](const std::string& v) { std::cout << v << std::endl; });
+    rpp::source::just(42)
+            | rpp::operators::map([](int value) { return std::to_string(value) + " VAL"; })
+            | rpp::operators::subscribe([](const std::string& v) { std::cout << v << std::endl; });
     // Output: 42 VAL
     //! [Changed type]
     return 0;
