67f7831319e39e071e29da0b04d1a0368353a104
Aleksey Loginov <victimsnino@gmail.com>
2024-05-11 23:18:35 +0300

    fix macos

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -110,7 +110,6 @@ endif()
 
 # ===================== Snitch ===================
 if (RPP_BUILD_TESTS)
-  SET(SNITCH_CONSTEXPR_FLOAT_USE_BITCAST 0)
   rpp_fetch_library(snitch https://github.com/cschreib/snitch.git v1.2.4)
 endif()
 
