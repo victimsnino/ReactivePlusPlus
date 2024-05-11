77a057dfa3fd93a6abd008c2dc980027caaa639f
Aleksey Loginov <victimsnino@gmail.com>
2024-05-12 00:25:57 +0300

    dump content of conan_toolchain

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -17,7 +17,9 @@ if(POLICY CMP0144)
 endif()
 
 # conan install . --output-folder=build --build=missing -s compiler.cppstd=20 -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
-# if(NOT DEFINED CMAKE_TOOLCHAIN_FILE AND EXISTS "${CMAKE_BINARY_DIR}/conan_toolchain.cmake")
-#     include(${CMAKE_BINARY_DIR}/conan_toolchain.cmake)
-#     set(CMAKE_TOOLCHAIN_FILE ${CMAKE_BINARY_DIR}/conan_toolchain.cmake)
-# endif()
+if(NOT DEFINED CMAKE_TOOLCHAIN_FILE AND EXISTS "${CMAKE_BINARY_DIR}/conan_toolchain.cmake")
+    # include(${CMAKE_BINARY_DIR}/conan_toolchain.cmake)
+    # set(CMAKE_TOOLCHAIN_FILE ${CMAKE_BINARY_DIR}/conan_toolchain.cmake)
+    file(READ ${CMAKE_BINARY_DIR}/conan_toolchain.cmake CONAN_DATA)
+    message(${CONAN_DATA})
+endif()
