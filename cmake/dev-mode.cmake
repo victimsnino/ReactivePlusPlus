35b2d6466c1ddd3fe1e3065e2b9bb03c3d798531
Aleksey Loginov <victimsnino@gmail.com>
2024-05-12 00:34:29 +0300

    try to fix macos

--- cmake/dev-mode.cmake
+++ cmake/dev-mode.cmake
@@ -18,8 +18,6 @@ endif()
 
 # conan install . --output-folder=build --build=missing -s compiler.cppstd=20 -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 if(NOT DEFINED CMAKE_TOOLCHAIN_FILE AND EXISTS "${CMAKE_BINARY_DIR}/conan_toolchain.cmake")
-    # include(${CMAKE_BINARY_DIR}/conan_toolchain.cmake)
-    # set(CMAKE_TOOLCHAIN_FILE ${CMAKE_BINARY_DIR}/conan_toolchain.cmake)
-    file(READ ${CMAKE_BINARY_DIR}/conan_toolchain.cmake CONAN_DATA)
-    message(${CONAN_DATA})
+    include(${CMAKE_BINARY_DIR}/conan_toolchain.cmake)
+    set(CMAKE_TOOLCHAIN_FILE ${CMAKE_BINARY_DIR}/conan_toolchain.cmake)
 endif()
