e66dca3210e7a7bfcf9e614891c2a07c0cf2ad2b
Aleksey Loginov <victimsnino@gmail.com>
2024-05-22 14:38:04 +0000

    one more

--- src/tests/utils/rpp_trompeloil.hpp
+++ src/tests/utils/rpp_trompeloil.hpp
@@ -5,6 +5,10 @@
 
 #include <trompeloeil.hpp>
 
+#include <rpp/disposables/fwd.hpp>
+#include <exception>
+
+
 namespace trompeloeil
 {
   template <>
@@ -36,3 +40,22 @@ namespace trompeloeil
       REQUIRE(trompeloeil_mock_calls_done_correctly != 0);
   }
 }
+
+template<typename T>
+struct mock_observer
+{
+    mock_observer() 
+    {
+        ALLOW_CALL(*this, is_disposed()).RETURN(false);
+        ALLOW_CALL(*this, set_upstream(trompeloeil::_));
+    }
+
+    static constexpr bool trompeloeil_movable_mock = true;
+
+    MAKE_MOCK1(on_next, void(const T&), const);
+    MAKE_MOCK1(on_next, void(T&&), const);
+    MAKE_MOCK1(on_error, void(const std::exception_ptr& err), const);
+    MAKE_MOCK0(on_completed, void(), const);
+    MAKE_MOCK0(is_disposed, bool(), const);
+    MAKE_MOCK1(set_upstream, void(const rpp::disposable_wrapper&), const);
+};
