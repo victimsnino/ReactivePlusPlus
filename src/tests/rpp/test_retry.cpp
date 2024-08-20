556ef37810e3cb772134af61920aa74007991130
Aleksey Loginov <victimsnino@gmail.com>
2024-08-20 23:49:34 +0300

    extend tests

--- src/tests/rpp/test_retry.cpp
+++ src/tests/rpp/test_retry.cpp
@@ -15,6 +15,7 @@
 #include <rpp/sources/concat.hpp>
 #include <rpp/sources/error.hpp>
 #include <rpp/sources/just.hpp>
+#include <rpp/sources/never.hpp>
 
 #include "copy_count_tracker.hpp"
 #include "disposable_observable.hpp"
@@ -56,6 +57,44 @@ TEST_CASE("retry handles errors properly")
             observable | rpp::operators::retry(2) | rpp::operators::subscribe(mock);
         }
     }
+    SECTION("observable 1-|")
+    {
+        const auto observable = rpp::source::just(1);
+
+        SECTION("retry(0)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_completed()).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(0) | rpp::operators::subscribe(mock);
+        }
+
+        SECTION("retry(2)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_completed()).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(2) | rpp::operators::subscribe(mock);
+        }
+    }
+    SECTION("observable 1->")
+    {
+        const auto observable = rpp::source::concat(rpp::source::just(1), rpp::source::never<int>());
+
+        SECTION("retry(0)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(0) | rpp::operators::subscribe(mock);
+        }
+
+        SECTION("retry(2)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(2) | rpp::operators::subscribe(mock);
+        }
+    }
 }
 
 TEST_CASE("retry handles stack overflow")
@@ -74,4 +113,27 @@ TEST_CASE("retry handles stack overflow")
     })
         | rpp::operators::retry(count)
         | rpp::operators::subscribe(mock);
+}
+
+TEST_CASE("retry doesn't produce extra copies")
+{
+    SECTION("retry(2)")
+    {
+        copy_count_tracker::test_operator(rpp::ops::retry(2),
+                                          {
+                                              .send_by_copy = {.copy_count = 1, // 1 copy to final subscriber
+                                                               .move_count = 0},
+                                              .send_by_move = {.copy_count = 0,
+                                                               .move_count = 1} // 1 move to final subscriber
+                                          });
+    }
+}
+
+TEST_CASE("retry satisfies disposable contracts")
+{
+    test_operator_with_disposable<int>(rpp::ops::retry(1));
+
+    test_operator_over_observable_with_disposable<int>([](const auto& observable) {
+        return rpp::source::concat(observable, rpp::source::error<int>({})) | rpp::ops::retry(10);
+    });
 }
