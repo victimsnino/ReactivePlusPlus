ff8d1499caf82fde25e3d47947a5cdd1da86d0b1
Aleksey Loginov <victimsnino@gmail.com>
2024-08-21 22:48:59 +0300

    implement simplest retry (#619)
    * implement simplest retry
    
    * fix
    
    * extend
    
    * add stack overflow test for repeat
    
    * extend tests
    
    * extend with infinite retry
    
    * add doxygen
    
    * benchmarks + doc
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * rename
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ src/rpp/rpp/operators/retry.hpp
@@ -0,0 +1,182 @@
+//                  ReactivePlusPlus library
+//
+//          Copyright Aleksey Loginov 2023 - present.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/victimsnino/ReactivePlusPlus
+//
+
+#pragma once
+
+#include <rpp/operators/fwd.hpp>
+
+#include <rpp/defs.hpp>
+#include <rpp/operators/details/strategy.hpp>
+
+namespace rpp::operators::details
+{
+    template<rpp::constraint::observer TObserver, constraint::decayed_type Observable>
+    struct retry_state_t final : public rpp::composite_disposable
+    {
+        retry_state_t(TObserver&& in_observer, const Observable& observable, std::optional<size_t> count)
+            : count{count}
+            , observer(std::move(in_observer))
+            , observable(observable)
+
+        {
+        }
+
+        std::optional<size_t> count;
+        std::atomic<bool>     is_inside_drain{};
+
+        RPP_NO_UNIQUE_ADDRESS TObserver  observer;
+        RPP_NO_UNIQUE_ADDRESS Observable observable;
+    };
+
+    template<rpp::constraint::observer TObserver, typename TObservable>
+    void drain(const std::shared_ptr<retry_state_t<TObserver, TObservable>>& state);
+
+    template<rpp::constraint::observer TObserver, typename TObservable>
+    struct retry_observer_strategy
+    {
+        using preferred_disposable_strategy = rpp::details::observers::none_disposable_strategy;
+
+        std::shared_ptr<retry_state_t<TObserver, TObservable>> state;
+        mutable bool                                           locally_disposed{};
+
+        template<typename T>
+        void on_next(T&& v) const
+        {
+            state->observer.on_next(std::forward<T>(v));
+        }
+
+        void on_error(const std::exception_ptr& err) const
+        {
+            locally_disposed = true;
+            if (state->count == 0)
+            {
+                state->observer.on_error(err);
+                state->dispose();
+                return;
+            }
+
+            if (state->is_inside_drain.exchange(false, std::memory_order::seq_cst))
+                return;
+
+            drain(state);
+        }
+
+        void on_completed() const
+        {
+            locally_disposed = true;
+            state->observer.on_completed();
+        }
+
+        void set_upstream(const disposable_wrapper& d) const
+        {
+            state->add(d);
+        }
+
+        bool is_disposed() const { return locally_disposed || state->is_disposed(); }
+    };
+
+    template<rpp::constraint::observer TObserver, typename TObservable>
+    void drain(const std::shared_ptr<retry_state_t<TObserver, TObservable>>& state)
+    {
+        while (!state->is_disposed())
+        {
+            if (state->count)
+                --state->count.value();
+            state->clear();
+            state->is_inside_drain.store(true, std::memory_order::seq_cst);
+            try
+            {
+                using value_type = rpp::utils::extract_observer_type_t<TObserver>;
+                state->observable.subscribe(observer<value_type, retry_observer_strategy<TObserver, TObservable>>{state});
+
+                if (state->is_inside_drain.exchange(false, std::memory_order::seq_cst))
+                    return;
+            }
+            catch (...)
+            {
+                state->observer.on_error(std::current_exception());
+                return;
+            }
+        }
+    }
+
+    struct retry_t
+    {
+        const std::optional<size_t> count{};
+
+        template<rpp::constraint::decayed_type T>
+        struct operator_traits
+        {
+            using result_type = T;
+        };
+
+        template<rpp::details::observables::constraint::disposable_strategy Prev>
+        using updated_disposable_strategy = rpp::details::observables::fixed_disposable_strategy_selector<1>;
+
+        template<rpp::constraint::observer TObserver, typename TObservable>
+        void subscribe(TObserver&& observer, TObservable&& observble) const
+        {
+            const auto d   = disposable_wrapper_impl<retry_state_t<std::decay_t<TObserver>, std::decay_t<TObservable>>>::make(std::forward<TObserver>(observer), std::forward<TObservable>(observble), count ? count.value() + 1 : count);
+            auto       ptr = d.lock();
+
+            ptr->observer.set_upstream(d.as_weak());
+            drain(ptr);
+        }
+    };
+} // namespace rpp::operators::details
+
+namespace rpp::operators
+{
+    /**
+     * @brief The retry operator attempts to resubscribe to the observable when an error occurs, up to the specified number of retries.
+     *
+     * @marble retry
+       {
+           source observable    : +-1-x
+           operator "retry:(2)" : +-1-1-1-x
+       }
+     *
+     * @param count is the number of retries
+     *
+     * @warning #include <rpp/operators/retry.hpp>
+     *
+     * @par Examples:
+     * @snippet retry.cpp retry
+     *
+     * @ingroup error_handling_operators
+     * @see https://reactivex.io/documentation/operators/retry.html
+     */
+    inline auto retry(size_t count)
+    {
+        return details::retry_t{count};
+    }
+
+    /**
+    * @brief The infinite retry operator continuously attempts to resubscribe to the observable upon error, without a retry limit.
+    *
+    * @marble infinite_retry
+      {
+          source observable    : +-1-x
+          operator "retry:()"  : +-1-1-1-1-1-1-1-1-1-1-1->
+      }
+    *
+    * @warning #include <rpp/operators/retry.hpp>
+    *
+    * @par Examples:
+    * @snippet retry.cpp retry_infinitely
+    *
+    * @ingroup error_handling_operators
+    * @see https://reactivex.io/documentation/operators/retry.html
+    */
+    inline auto retry()
+    {
+        return details::retry_t{};
+    }
+} // namespace rpp::operators
---
+++ src/tests/rpp/test_retry.cpp
@@ -0,0 +1,178 @@
+//                  ReactivePlusPlus library
+//
+//          Copyright Aleksey Loginov 2023 - present.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+//
+// Project home: https://github.com/victimsnino/ReactivePlusPlus
+//
+
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include <rpp/operators/retry.hpp>
+#include <rpp/sources/concat.hpp>
+#include <rpp/sources/error.hpp>
+#include <rpp/sources/just.hpp>
+#include <rpp/sources/never.hpp>
+
+#include "copy_count_tracker.hpp"
+#include "disposable_observable.hpp"
+#include "rpp_trompeloil.hpp"
+
+TEST_CASE("retry handles errors properly")
+{
+    mock_observer<int>    mock{};
+    trompeloeil::sequence seq;
+
+    SECTION("observable 1-x-2")
+    {
+        const auto observable = rpp::source::concat(rpp::source::just(1), rpp::source::error<int>({}), rpp::source::just(2));
+
+        SECTION("retry(0)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_error(trompeloeil::_)).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(0) | rpp::operators::subscribe(mock);
+        }
+
+        SECTION("retry(1)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_error(trompeloeil::_)).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(1) | rpp::operators::subscribe(mock);
+        }
+
+        SECTION("retry(2)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_error(trompeloeil::_)).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(2) | rpp::operators::subscribe(mock);
+        }
+
+        SECTION("retry()")
+        {
+            auto d = rpp::composite_disposable_wrapper::make();
+
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).LR_SIDE_EFFECT(d.dispose()).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry() | rpp::operators::subscribe(d, mock);
+        }
+    }
+    SECTION("observable 1-|")
+    {
+        const auto observable = rpp::source::just(1);
+
+        SECTION("retry(0)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_completed()).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(0) | rpp::operators::subscribe(mock);
+        }
+
+        SECTION("retry(2)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_completed()).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(2) | rpp::operators::subscribe(mock);
+        }
+
+        SECTION("retry()")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+            REQUIRE_CALL(*mock, on_completed()).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry() | rpp::operators::subscribe(mock);
+        }
+    }
+    SECTION("observable 1->")
+    {
+        const auto observable = rpp::source::concat(rpp::source::just(1), rpp::source::never<int>());
+
+        SECTION("retry(0)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(0) | rpp::operators::subscribe(mock);
+        }
+
+        SECTION("retry(2)")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry(2) | rpp::operators::subscribe(mock);
+        }
+
+        SECTION("retry()")
+        {
+            REQUIRE_CALL(*mock, on_next_lvalue(1)).IN_SEQUENCE(seq);
+
+            observable | rpp::operators::retry() | rpp::operators::subscribe(mock);
+        }
+    }
+}
+
+TEST_CASE("retry handles stack overflow")
+{
+    mock_observer<int>    mock{};
+    trompeloeil::sequence seq;
+
+    constexpr size_t count = 500000;
+
+    REQUIRE_CALL(*mock, on_next_rvalue(trompeloeil::_)).TIMES(count + 1).IN_SEQUENCE(seq);
+    REQUIRE_CALL(*mock, on_error(trompeloeil::_)).IN_SEQUENCE(seq);
+
+    rpp::source::create<int>([](const auto& obs) {
+        obs.on_next(1);
+        obs.on_error({});
+    })
+        | rpp::operators::retry(count)
+        | rpp::operators::subscribe(mock);
+}
+
+TEST_CASE("retry doesn't produce extra copies")
+{
+    SECTION("retry(2)")
+    {
+        copy_count_tracker::test_operator(rpp::ops::retry(2),
+                                          {
+                                              .send_by_copy = {.copy_count = 1, // 1 copy to final subscriber
+                                                               .move_count = 0},
+                                              .send_by_move = {.copy_count = 0,
+                                                               .move_count = 1} // 1 move to final subscriber
+                                          });
+    }
+    SECTION("retry()")
+    {
+        copy_count_tracker::test_operator(rpp::ops::retry(),
+                                          {
+                                              .send_by_copy = {.copy_count = 1, // 1 copy to final subscriber
+                                                               .move_count = 0},
+                                              .send_by_move = {.copy_count = 0,
+                                                               .move_count = 1} // 1 move to final subscriber
+                                          });
+    }
+}
+
+TEST_CASE("retry satisfies disposable contracts")
+{
+    test_operator_with_disposable<int>(rpp::ops::retry(1));
+
+    test_operator_over_observable_with_disposable<int>([](const auto& observable) {
+        return rpp::source::concat(observable, rpp::source::error<int>({})) | rpp::ops::retry(10);
+    });
+}
