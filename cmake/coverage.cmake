5108dab8f304ce9c43d7ea3464519ef161c4682d
Aleksey Loginov <victimsnino@gmail.com>
2024-10-07 22:42:21 +0300

    Update coverage.cmake

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -8,7 +8,7 @@ set(RPP_COVERAGE_TARGETS -instr-profile=${RPP_TEST_RESULTS_DIR}/results.profdata
 
 add_custom_target(
     coverage
-    COMMAND llvm-profdata merge -sparse=true ${RPP_TEST_RESULTS_DIR}/*.profraw -o ${RPP_TEST_RESULTS_DIR}/results.profdata
+    COMMAND llvm-profdata merge ${RPP_TEST_RESULTS_DIR}/*.profraw -o ${RPP_TEST_RESULTS_DIR}/results.profdata
 
     COMMAND llvm-cov report --ignore-filename-regex=build|tests ${RPP_COVERAGE_TARGETS}
     COMMAND llvm-cov show --ignore-filename-regex=build --show-branches=count --show-expansions --show-line-counts --show-line-counts-or-regions --show-regions ${RPP_COVERAGE_TARGETS} > ${RPP_TEST_RESULTS_DIR}/coverage.txt
