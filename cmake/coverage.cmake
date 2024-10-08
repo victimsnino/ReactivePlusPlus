b5bd748650136ac0474d19b86387ca47f35255ee
Aleksey Loginov <victimsnino@gmail.com>
2024-10-08 17:09:11 +0300

    Add rppasio to coverage (#656)
    * Add rppasio to coverage
    
    * fix install

--- cmake/coverage.cmake
+++ cmake/coverage.cmake
@@ -4,7 +4,7 @@ if (NOT RPP_BUILD_TESTS_TOGETHER)
     message( FATAL_ERROR "Expected to set RPP_BUILD_TESTS_TOGETHER flag when build coverage via llvm cov")
 endif()
 
-set(RPP_COVERAGE_TARGETS -instr-profile=${RPP_TEST_RESULTS_DIR}/results.profdata -object $<TARGET_FILE:test_rpp> -object $<TARGET_FILE:test_rppqt> -object $<TARGET_FILE:test_rppgrpc>)
+set(RPP_COVERAGE_TARGETS -instr-profile=${RPP_TEST_RESULTS_DIR}/results.profdata -object $<TARGET_FILE:test_rpp> -object $<TARGET_FILE:test_rppqt> -object $<TARGET_FILE:test_rppgrpc> -object $<TARGET_FILE:test_rppasio>)
 
 add_custom_target(
     coverage
