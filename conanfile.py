9581875cf10a4e877d83c4f37c6853325c41fad6
Aleksey Loginov <victimsnino@gmail.com>
2025-09-03 23:36:01 +0300

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -32,7 +32,7 @@ class RppConan(ConanFile):
             self.requires("nanobench/4.3.11")
 
         if self.options.with_sfml:
-            self.requires("sfml/2.6.1", options={"audio": False})
+            self.requires("sfml/2.6.2", options={"audio": False})
 
         if self.options.with_grpc:
             self.requires("grpc/1.54.3", transitive_libs=True, transitive_headers=True)
