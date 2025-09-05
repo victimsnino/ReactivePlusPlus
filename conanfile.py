394508defe4d0148cba9bcea3808df5432a6e254
Aleksey Loginov <victimsnino@gmail.com>
2025-09-05 15:49:20 +0300

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -24,17 +24,7 @@ class RppConan(ConanFile):
     }
 
     def configure(self):
-        self.options["grpc/*"].codegen = False
-        self.options["grpc/*"].cpp_plugin = True
-        self.options["grpc/*"].csharp_ext = False
-        self.options["grpc/*"].php_plugin = False
-        self.options["grpc/*"].node_plugin = False
-        self.options["grpc/*"].otel_plugin = False
-        self.options["grpc/*"].ruby_plugin = False
-        self.options["grpc/*"].csharp_plugin = False
-        self.options["grpc/*"].python_plugin = False
         self.options["grpc/*"].with_libsystemd = False
-        self.options["grpc/*"].objective_c_plugin = False
 
     def requirements(self):
         if self.options.with_tests:
