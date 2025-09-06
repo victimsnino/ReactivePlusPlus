7a75fbb419861569069b61e876e55da7bcbca8b0
Aleksey Loginov <victimsnino@gmail.com>
2025-09-06 12:12:57 +0300

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -54,11 +54,3 @@ class RppConan(ConanFile):
         if self.options.with_cmake:
             self.tool_requires("cmake/3.29.3")
 
-    def generate(self):
-        deps = CMakeDeps(self)
-        deps.generate()
-        tc = CMakeToolchain(self)
-
-        if self.settings.compiler == "apple-clang":
-            tc.extra_cxxflags.extend(['-Wno-missing-template-arg-list-after-template-kw'])
-        tc.generate()
