8b9912d309716e6e1b9e3b3a2a3ddb2e0fbe9d4f
Aleksey Loginov <victimsnino@gmail.com>
2025-09-09 21:56:14 +0300

    fix ci2 (#722)
    * jinja2
    
    * Update ci v2.yml
    
    * Update ci v2.yml
    
    * Update ci v2.yml
    
    * Update ci v2.yml
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update ci v2.yml
    
    * Update ci v2.yml
    
    * Update ci v2.yml
    
    * Update ci v2.yml
    
    * Update ci v2.yml
    
    * Update ci v2.yml
    
    * Update conanfile.py
    
    * Update ci v2.yml
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update CMakePresets.json
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * Update conanfile.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update conanfile.py
    
    * sfml
    
    * disable windows for now
    
    * Update analyzers.yml
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- conanfile.py
+++ conanfile.py
@@ -23,6 +23,15 @@ class RppConan(ConanFile):
         "with_asio" : False
     }
 
+    def configure(self):
+        self.options["grpc/*"].with_libsystemd = False
+        self.options["grpc/*"].csharp_plugin = False
+        self.options["grpc/*"].node_plugin = False
+        self.options["grpc/*"].objective_c_plugin = False
+        self.options["grpc/*"].php_plugin = False
+        self.options["grpc/*"].python_plugin = False
+        self.options["grpc/*"].ruby_plugin = False
+
     def requirements(self):
         if self.options.with_tests:
             self.requires("trompeloeil/48")
@@ -31,12 +40,12 @@ class RppConan(ConanFile):
         if self.options.with_benchmarks:
             self.requires("nanobench/4.3.11")
 
-        if self.options.with_sfml:
-            self.requires("sfml/2.6.1", options={"audio": False})
+        # if self.options.with_sfml:
+        #     self.requires("sfml/2.6.2", options={"audio": False})
 
         if self.options.with_grpc:
-            self.requires("grpc/1.54.3", transitive_libs=True, transitive_headers=True)
-            self.requires("protobuf/3.21.12")
+            self.requires("grpc/1.65.0", transitive_libs=True, transitive_headers=True)
+            # self.requires("protobuf/5.26.1")
             self.requires("libmount/2.39", override=True)
 
         if self.options.with_asio:
