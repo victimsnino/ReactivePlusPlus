968d315ee91847bdedb86f3299f460b06d468871
Aleksey Loginov <victimsnino@gmail.com>
2026-02-13 20:21:17 +0300

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -44,9 +44,9 @@ class RppConan(ConanFile):
         #     self.requires("sfml/2.6.2", options={"audio": False})
 
         if self.options.with_grpc:
-            self.requires("grpc/1.67.1", transitive_libs=True, transitive_headers=True)
+            self.requires("grpc/1.65.0", transitive_libs=True, transitive_headers=True)
             # self.requires("protobuf/6.32.1")
-            # self.requires("libmount/2.39", override=True)
+            self.requires("libmount/2.39", override=True)
 
         if self.options.with_asio:
             self.requires("asio/1.30.2")
